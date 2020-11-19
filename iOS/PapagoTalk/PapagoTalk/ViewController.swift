e3288e64176805e33a10766921ffcbf126fea25d
Minkwan Song <minkwan1120@naver.com>
2020-11-19 18:43:11 +0900

    feat : iOS 프로젝트 생성
    Issue #8

---
+++ iOS/PapagoTalk/PapagoTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
