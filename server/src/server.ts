eef837f523e39a6fc65a7c33a7c4ba05e8782279
Taegyung Gong <gtg92t@gmail.com>
2020-11-19 18:58:48 +0900

    Chore : Add morgan & change graphql server params - use morgan to check server log - update GraphQLServer params schema

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,13 @@
 import './env';
 import { GraphQLServer } from 'graphql-yoga';
+import schema from './schema';
+import logger from 'morgan';
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-  type Query {
-    hello: String!
-  } 
-`;
+const server = new GraphQLServer({ schema });
 
-const resolvers = {
-  Query: {
-    hello: (): string => 'hello',
-  },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+server.express.use(logger('dev'));
 
 server.start({ port: PORT, cors: { origin: '*' } }, () => {
   console.log(`Server running on http://localhost:${PORT}`);
