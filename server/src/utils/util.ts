983d2e25847281acd9d47aa99f61eb9f04503dad
johnyejin <johnyejin@kakao.com>
2020-11-21 16:59:46 +0900

    fix : 랜덤 숫자 생성 함수에 자릿수 매개변수 추가
    Issue #24

--- server/src/utils/util.ts
+++ server/src/utils/util.ts
@@ -2,9 +2,9 @@ const fillZero = (num: string, width: number): string => {
   return '0'.repeat(width - num.length) + num;
 };
 
-const getRandomRoomNumber = (): string => {
-  const randomNum = Math.floor(Math.random() * 1000000);
-  return fillZero(String(randomNum), 6);
+const getRandomNumber = (width: number): string => {
+  const randomNum = Math.floor(Math.random() * 10 ** width);
+  return fillZero(String(randomNum), width);
 };
 
-export default { getRandomRoomNumber };
+export default { getRandomNumber };
