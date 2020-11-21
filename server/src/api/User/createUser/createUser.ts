fddbf7f2293e0dc73f6e9df393f7c74a6a7cd8e7
Taegyung Gong <gtg92t@gmail.com>
2020-11-21 17:56:46 +0900

    Feat: Update create user api - User 생성 시 roome을 같이 만들도록 구현 - 생성한 room에 자동으로 참여자를 방금 생성한 유저 추가 - return 값으로 생선한 random code를 반환함

--- server/src/api/User/createUser/createUser.ts
+++ server/src/api/User/createUser/createUser.ts
@@ -1,21 +1,35 @@
 import { PrismaClient } from '@prisma/client';
-
+import getRandomNumber from '../../../utils/util';
 const prisma = new PrismaClient();
 
 export default {
   Mutation: {
     createUser: async (_: any, args: any) => {
-      const { nickname, avatar, password = '', lang } = args;
-      const user = await prisma.user.create({
-        data: {
-          nickname,
-          avatar,
-          lang,
-          password,
-        },
-      });
-      if (user) return true;
-      return false;
+      const { nickname, avatar, lang } = args;
+      try {
+        const user = await prisma.user.create({
+          data: {
+            nickname,
+            lang,
+            avatar,
+          },
+        });
+        const randomCode = getRandomNumber(6);
+        await prisma.room.create({
+          data: {
+            users: {
+              connect: {
+                id: user.id,
+              },
+            },
+            avatar: 'https://picsum.photos/200',
+            code: randomCode,
+          },
+        });
+        return randomCode;
+      } catch (error) {
+        throw new Error('error');
+      }
     },
   },
 };
