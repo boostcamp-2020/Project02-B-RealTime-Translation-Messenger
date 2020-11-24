358171b6bda37e30f3af032166cbea08ee2ae50b
ByoungHwi <1994blyo@gamil.com>
2020-11-25 00:36:30 +0900

    Feat : Locale 및 UIViewController extension 구현 - 현재 기기에서 사용중인 언어를 확인하기 위한 extension 구현 - UIViewController의 파일명을 identifier로 가져오는 extension 구현

---
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/Locale+.swift
@@ -0,0 +1,26 @@
+//
+//  Locale+.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/25.
+//
+
+import Foundation
+
+extension Locale {
+    static var preferredLanguageCode: String {
+        let defaultLanguage = "ko"
+        let preferredLanguage = preferredLanguages.first ?? defaultLanguage
+        return Locale(identifier: preferredLanguage).languageCode ?? defaultLanguage
+    }
+    static var currentLanguage: Language {
+        switch preferredLanguageCode {
+        case "ko":
+            return .korean
+        case "en":
+            return .english
+        default:
+            return .english
+        }
+    }
+}
---
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/UIViewController+.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/24.
+//
+
+import UIKit
+import RxSwift
+
+extension UIViewController {
+    
+    static var identifier: String {
+        String(describing: Self.self)
+    }
+}
