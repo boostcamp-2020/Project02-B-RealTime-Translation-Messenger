31ab538b8f8d9cd0a30aaf14652fd3bcc75b977a
Minkwan Song <minkwan1120@naver.com>
2020-11-25 01:26:58 +0900

    Feat : ChatViewController ReactorKit 구조 틀 구현
    Issue #48

--- iOS/PapagoTalk/PapagoTalk/Chat/ChatViewController.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/ChatViewController.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
 
-class ChatViewController: UIViewController {
+final class ChatViewController: UIViewController, StoryboardView {
 
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        reactor = ChatViewReactor()
     }
     
+    func bind(reactor: ChatViewReactor) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
