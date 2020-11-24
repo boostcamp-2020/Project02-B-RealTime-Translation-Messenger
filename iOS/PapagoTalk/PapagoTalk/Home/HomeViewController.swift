1bda58207c473bd431b6457069157a1316791fdb
ByoungHwi <1994blyo@gamil.com>
2020-11-24 19:44:16 +0900

    Feat : Home ViewController 프로필이미지 기능 구현 - 프로필 ImageView에 탭 제스쳐 추가 - 탭 제스쳐와 Reactor 바인딩 - Reactor State와 프로필 ImageView 바인딩 - 이미지 캐싱을 위한 Kingfisher 추가
    Issue #36

--- iOS/PapagoTalk/PapagoTalk/Home/HomeViewController.swift
+++ iOS/PapagoTalk/PapagoTalk/Home/HomeViewController.swift
@@ -8,15 +8,19 @@
 import UIKit
 import ReactorKit
 import RxCocoa
+import Kingfisher
 
 final class HomeViewController: UIViewController, StoryboardView {
     
     @IBOutlet weak var nickNameTextField: UITextField!
+    @IBOutlet weak var profileImageView: UIImageView!
     
     var disposeBag = DisposeBag()
+    var profileImageTapGesture =  UITapGestureRecognizer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileImageView.addGestureRecognizer(profileImageTapGesture)
         reactor = HomeViewReactor()
     }
     
@@ -27,6 +31,11 @@ final class HomeViewController: UIViewController, StoryboardView {
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
+        profileImageTapGesture.rx.event
+            .map { _ in Reactor.Action.profileImageTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
         reactor.state.map { $0.nickName }
             .bind(to: nickNameTextField.rx.text)
             .disposed(by: disposeBag)
@@ -34,6 +43,14 @@ final class HomeViewController: UIViewController, StoryboardView {
         reactor.state.map { $0.isInvalidNickNameLength }
             .do { if $0 { self.nickNameTextField.shake() } }
             .subscribe()
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.profileImageURL }
+            .distinctUntilChanged()
+            .compactMap { URL(string: $0) }
+            .subscribe(onNext: { [weak self] in
+                self?.profileImageView.kf.setImage(with: $0)
+            })
             .disposed(by: disposeBag)
     }
 }
