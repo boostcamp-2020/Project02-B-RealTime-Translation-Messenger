6b937bbbdb604612745913d562fac3acc756685a
Minkwan Song <minkwan1120@naver.com>
2020-11-25 01:25:38 +0900

    Feat : Chat Scene ViewController, Reactor 파일 및 폴더 생성
    Issue #48

---
+++ iOS/PapagoTalk/PapagoTalk/Chat/ChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatViewController.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/25.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/PapagoTalk/PapagoTalk/Chat/ChatViewReactor.swift
@@ -0,0 +1,8 @@
+//
+//  ChatViewReactor.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/25.
+//
+
+import Foundation
