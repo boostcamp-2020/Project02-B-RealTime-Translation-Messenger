6ee370220da59359cb3e534cf605251a036fc500
ByoungHwi <1994blyo@gamil.com>
2020-11-24 14:50:09 +0900

    Feat : HomeViewController 구현 - 닉네임텍스트필드 관련 바인딩
    Issue #37

--- iOS/PapagoTalk/PapagoTalk/Home/HomeViewController.swift
+++ iOS/PapagoTalk/PapagoTalk/Home/HomeViewController.swift
@@ -6,11 +6,28 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, StoryboardView {
+    var disposeBag = DisposeBag()
+    
+    @IBOutlet weak var nickNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        reactor = HomeReactor()
     }
-
+    
+    func bind(reactor: HomeReactor) {
+        nickNameTextField.rx.text
+            .orEmpty
+            .map { Reactor.Action.nickNameChanged($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.nickName }
+            .bind(to: nickNameTextField.rx.text)
+            .disposed(by: disposeBag)
+    }
 }
