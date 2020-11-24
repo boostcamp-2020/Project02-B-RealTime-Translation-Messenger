6fc2f4d831aed779f4af5716d6f3788260df0e9b
ByoungHwi <1994blyo@gamil.com>
2020-11-24 19:40:27 +0900

    Feat : 랜덤 이미지 URL 제공을 위한 팩토리 구현 - 이미지 URL String을 static으로 관리하고, randomElement를 출력하는 함수 구현 - Testable한 구조를 위한 프로토콜 추상화
    Issue #36

--- iOS/PapagoTalk/PapagoTalk/Home/DefaultImageFactory.swift
+++ iOS/PapagoTalk/PapagoTalk/Home/DefaultImageFactory.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct DefaultImageFactory: ImageFactory {
+    
+    private static let defaultImages = [
+        "https://kr.object.ncloudstorage.com/papagotalk/skyblue.png",
+        "https://kr.object.ncloudstorage.com/papagotalk/red.png",
+        "https://kr.object.ncloudstorage.com/papagotalk/orange.png",
+        "https://kr.object.ncloudstorage.com/papagotalk/gray.png"
+    ]
+    
+    func randomImageURL() -> String {
+        guard let random = Self.defaultImages.randomElement() else {
+            return ""
+        }
+        return random
+    }
+}
