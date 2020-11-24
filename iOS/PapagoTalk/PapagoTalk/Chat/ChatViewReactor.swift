f77bc94863bdbf30840f7d4ecebd71a874a1a66d
Minkwan Song <minkwan1120@naver.com>
2020-11-25 01:28:00 +0900

    Feat : ChatViewReactor ReactorKit 구조 틀 구현
    Issue #48

--- iOS/PapagoTalk/PapagoTalk/Chat/ChatViewReactor.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/ChatViewReactor.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import ReactorKit
+
+final class ChatViewReactor: Reactor {
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+      
+    }
+    
+    let initialState: State
+    
+    init() {
+        initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        
+        switch mutation {
+        }
+        return state
+    }
+}
