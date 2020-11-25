166f509883815c51dda8f0e85c3b011ce6358595
Minkwan Song <minkwan1120@naver.com>
2020-11-25 14:18:59 +0900

    Feat : ChatViewController inputBarTextView Height 변화 기능 - IBOutlet 선언(texView, textViewHeight) - textView의 text 크기에 따라 높이를 계산하는 calculateTextViewHeight() 함수 구현 - textView의 height에 textViewHeight constraint binding
    Issue #48

--- iOS/PapagoTalk/PapagoTalk/Chat/ChatViewController.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/ChatViewController.swift
@@ -11,14 +11,36 @@ import RxCocoa
 
 final class ChatViewController: UIViewController, StoryboardView {
 
+    @IBOutlet private weak var inputBarTextView: UITextView!
+    @IBOutlet private weak var inputBarTextViewHeight: NSLayoutConstraint!
+    
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         reactor = ChatViewReactor()
+        bind()
     }
     
     func bind(reactor: ChatViewReactor) {
-
+        
+    }
+    
+    private func bind() {
+        inputBarTextView.rx.text
+            .orEmpty
+            .compactMap { [weak self] text in
+                self?.calculateTextViewHeight(with: text)
+            }
+            .asObservable()
+            .distinctUntilChanged()
+            .bind(to: inputBarTextViewHeight.rx.constant)
+            .disposed(by: disposeBag)
+    }
+    
+    private func calculateTextViewHeight(with text: String) -> CGFloat {
+        let size = inputBarTextView.sizeThatFits(CGSize(width: inputBarTextView.bounds.size.width,
+                                                        height: CGFloat.greatestFiniteMagnitude))
+        return min(Constant.inputBarTextViewMaxHeight, size.height)
     }
 }
