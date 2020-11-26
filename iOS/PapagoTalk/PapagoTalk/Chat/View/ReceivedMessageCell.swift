cf71aa9887af388915c16e6b60fd824504dc8f76
ByoungHwi <1994blyo@gamil.com>
2020-11-27 01:31:21 +0900

    Feat : 테스트를 위한 메시지Cell 임시 구현 - 소켓통신 테스트를 위하여 TextView만을 가지고 있는 Cell 구현 - 사용자가 보낸 메시지와 받은 메시지를 표시할 Cell을 별도로 구현
    Issue #46

---
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/ReceivedMessageCell.swift
@@ -0,0 +1,12 @@
+//
+//  ReceivedMessageCell.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/27.
+//
+
+import UIKit
+
+class ReceivedMessageCell: UICollectionViewCell {
+    @IBOutlet weak var messageTextView: UITextView!
+}
---
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/SentMessageCell.swift
@@ -0,0 +1,12 @@
+//
+//  SentMessageCell.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/11/27.
+//
+
+import UIKit
+
+class SentMessageCell: UICollectionViewCell {
+    @IBOutlet weak var messageTextView: UITextView!
+}
