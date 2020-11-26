10ebdd299fdbb2980805ec91e5dc92435741ed38
Taegyung Gong <gtg92t@gmail.com>
2020-11-26 15:13:49 +0900

    Fix : Fix bug that prisma not support delete cacade in relation table - prisma에서 관계테이블 데이터 삭제 시 delete cacade 처리가 잘 적용되지   않는 버그
    Issue #81

--- server/src/api/User/deleteUser/deleteUser.ts
+++ server/src/api/User/deleteUser/deleteUser.ts
@@ -1,39 +1,19 @@
 import { PrismaClient } from '@prisma/client';
-import errorMessage from '@utils/errorMessage';
 
 const prisma = new PrismaClient();
 
 export default {
   Mutation: {
-    deleteUser: async (_: any, { id: userId }: { id: number }): Promise<boolean> => {
-      try {
-        /**
-         * 1. 유저 삭제 (삭제할때 해당 유저가 속한 방번호 조회)
-         * 2. 유저가 속했던 방에 남은 유저가 몇명 있는지 조회해서
-         * 3. 해당 방에 유저가 0명 있으면 방도 같이 지우기
-         */
-        const deleteUserResult = await prisma.user.delete({
-          where: { id: userId },
-          include: { rooms: true },
-        });
-
-        const roomId = deleteUserResult.rooms[0].id;
-
-        const afterRoom = await prisma.room.findOne({
-          where: { id: roomId },
-          include: { users: true },
-        });
-
-        if (afterRoom?.users.length === 0) {
-          await prisma.room.delete({
-            where: { id: roomId },
-          });
-        }
-
-        return true;
-      } catch (error) {
-        throw new Error(errorMessage.delete);
+    deleteUser: async (
+      _: any,
+      { userId, roomId }: { userId: number; roomId: number },
+    ): Promise<boolean> => {
+      await prisma.$queryRaw`DELETE FROM User WHERE id = ${userId}`;
+      const restUser = await prisma.$queryRaw`SELECT COUNT(*) FROM _RoomToUser WHERE A = ${roomId} `;
+      if (!restUser[0]['COUNT(*)']) {
+        await prisma.$queryRaw`DELETE FROM Room WHERE id = ${roomId}`;
       }
+      return true;
     },
   },
 };
