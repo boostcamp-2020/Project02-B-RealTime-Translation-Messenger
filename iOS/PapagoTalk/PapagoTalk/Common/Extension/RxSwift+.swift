a7a365705380e0bfd21d543ece890df433c0b5dc
Minkwan Song <minkwan1120@naver.com>
2020-11-27 03:11:54 +0900

    Style : RxSwift Extensions (ViewController ViewDidLoad, ViewWillAppear) observable return
    Issue #46

--- iOS/PapagoTalk/PapagoTalk/Common/Extension/RxSwift+.swift
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/RxSwift+.swift
@@ -5,4 +5,20 @@
 //  Created by 송민관 on 2020/11/27.
 //
 
-import Foundation
+import RxSwift
+
+///
+/// 출처 : https://github.com/pixeldock/RxAppState
+///
+extension RxSwift.Reactive where Base: UIViewController {
+    
+    var viewDidLoad: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewDidLoad))
+            .map { _ in return }
+    }
+    
+    var viewWillAppear: Observable<Bool> {
+        return methodInvoked(#selector(UIViewController.viewWillAppear))
+            .map { $0.first as? Bool ?? false }
+    }
+}
