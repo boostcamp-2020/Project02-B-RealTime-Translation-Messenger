51a037d46acdbd44e66d7a7f22d03e1d70b0dc4e
Minkwan Song <minkwan1120@naver.com>
2020-11-29 19:54:47 +0900

    Feat : UserDefault propertyWrapper 구현 - init type 으로 UserDefaultType - default Value는 Generic으로 구현 - get할 때는 UserDefaults에서 decode해서 값을 가져오고, set할 때는 UserDefault에 encode해서 값 저장
    Issue #96

---
+++ iOS/PapagoTalk/PapagoTalk/Entity/UserDefault.swift
@@ -0,0 +1,33 @@
+//
+//  UserDefault.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/29.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T: Codable> {
+    private let key: String
+    private let defaultValue: T
+    
+    init(type: UserDefaultType, default: T) {
+        key = type.key
+        defaultValue = `default`
+    }
+    
+    var wrappedValue: T {
+        get {
+            guard let data = UserDefaults.standard.object(forKey: key) as? Data,
+                  let value: T = try? data.decoded()
+            else {
+                return defaultValue
+            }
+            return value
+        }
+        set {
+            UserDefaults.standard.set(newValue.encoded(), forKey: key)
+        }
+    }
+}
