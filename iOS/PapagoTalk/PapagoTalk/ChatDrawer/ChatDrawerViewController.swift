5a4251ff6527271e046611a7c3f7fe8debe2eb49
Minkwan Song <minkwan1120@naver.com>
2020-11-30 02:08:21 +0900

    Feat : ChatViewDrawerViewController 생성 및 StoryboardView 채택 및 기본 틀 생성
    Issue #57

---
+++ iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChatDrawerViewController.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/30.
+//
+
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class ChatDrawerViewController: UIViewController, StoryboardView {
+    
+    var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func bind(reactor: ChatDrawerViewReactor) {
+        
+    }
+}
