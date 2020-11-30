62d06ab032ec52afaf0520cc1955e397aa609d2d
Minkwan Song <minkwan1120@naver.com>
2020-11-30 16:20:39 +0900

    Feat : ChatDrawerUserCell 생성 및 IBOutlet 연결
    Issue #57

---
+++ iOS/PapagoTalk/PapagoTalk/ChatDrawer/View/ChatDrawerUserCell.swift
@@ -0,0 +1,16 @@
+//
+//  ChatDrawerUserCell.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/30.
+//
+
+import UIKit
+
+final class ChatDrawerUserCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var profileImageView: UIImageView!
+    @IBOutlet private weak var nickNameLabel: UILabel!
+    @IBOutlet private weak var languageLabel: UILabel!
+    
+}
