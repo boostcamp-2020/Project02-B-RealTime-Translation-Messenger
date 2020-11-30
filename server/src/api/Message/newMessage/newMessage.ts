7afeae1eaccc090095717600d4a2d13a5612c66c
Taegyung Gong <gtg92t@gmail.com>
2020-11-30 16:22:16 +0900

    Feat : Update subscribe message API - subscribe 시 구독하는 사람의 언어를 함께 데이터로 넘겨 줌 - 구독하는 사람의 언어에 맞게 번역하여 결과를 json 형태의 문자로 리턴함   ex) 구독하는 사람 언어: ko => { origin: 'hi', translatedText: '안녕' }
    Issue #104

--- server/src/api/Message/newMessage/newMessage.ts
+++ server/src/api/Message/newMessage/newMessage.ts
@@ -1,12 +1,24 @@
 import { withFilter } from 'graphql-subscriptions';
+import req from '@utils/request';
 
 export default {
   Subscription: {
     newMessage: {
       subscribe: withFilter(
         (_: any, __: any, { pubsub }: any) => pubsub.asyncIterator('NEW_MESSAGE'),
-        (payload, variables) => {
-          return payload.newMessage.roomId === variables.roomId;
+        async (payload, variables): Promise<boolean> => {
+          if (payload.newMessage.roomId === variables.roomId) {
+            const { text, source } = payload.newMessage;
+            const target = variables.lang;
+            const translatedText = await req(text, source, target);
+            const texts = {
+              originText: text,
+              translatedText,
+            };
+            payload.newMessage.text = JSON.stringify(texts);
+            return true;
+          }
+          return false;
         },
       ),
     },
