c63f3bd4eb9eca19dcf6daadc7389f9a751931db
Minkwan Song <minkwan1120@naver.com>
2020-12-01 02:20:12 +0900

    Feat : MessageCell 프로토콜 하위 클래스들이 메소드 추상화 - Sent 및 Received Cell에서 공통으로 사용하는 함수 추상화를 위해 MessageCell extension에서 구현 - configureDate, configureMessage, configureTime 구현(각각의 UI와 필요한 데이터를 입력받아 구현) - convertToDateFormat, convertToTimeFormat 구현
    Issue #46

--- iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell.swift
@@ -7,6 +7,29 @@
 
 import UIKit
 
-protocol MessageCell: class {
+protocol MessageCell: UICollectionViewCell {
     func configureMessageCell(message: Message)
+}
+
+extension MessageCell {
+    func configureDate(of dateButton: UIButton, with timeStamp: Date, isFirst: Bool) {
+        dateButton.setTitle(convertToDateFormat(of: timeStamp), for: .normal)
+        dateButton.isHidden = !isFirst
+    }
+    
+    func configureMessage(of messageTextView: UITextView, with message: String) {
+        messageTextView.text = message
+    }
+    
+    func configureTime(of timeLabel: UILabel, with timeStamp: Date) {
+        timeLabel.text = convertToTimeFormat(of: timeStamp)
+    }
+    
+    private func convertToDateFormat(of timeStamp: Date) -> String {
+        return DateFormatter.chatDateFormat(of: timeStamp)
+    }
+    
+    private func convertToTimeFormat(of timeStamp: Date) -> String {
+        return DateFormatter.chatTimeFormat(of: timeStamp)
+    }
 }
