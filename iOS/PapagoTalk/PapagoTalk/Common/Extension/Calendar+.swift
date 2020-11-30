5e98e04c70c8e2e3b88bdf4611729a0af750de08
Minkwan Song <minkwan1120@naver.com>
2020-12-01 02:11:58 +0900

    Feat : Calendar Extension 추가 - timeStamp의 날짜를 비교하여, 채팅방에 새로운 날짜를 띄워주기 위해 isSameDate 함수 구현 - 메모리비용이 비싼 Calendar 객체 static으로 생성 및 static 함수에서 사용
    Issue #46

---
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/Calendar+.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar+.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/30.
+//
+
+import Foundation
+
+extension Calendar {
+    
+    static let calendar = Calendar(identifier: Calendar.current.identifier)
+
+    static func isSameDate(of firstDate: Date, with secondDate: Date) -> Bool {
+        return calendar.isDate(firstDate, inSameDayAs: secondDate)
+    }
+}
