12a22a95d05adfdedc237876a009d5dd85f9ab12
Minkwan Song <minkwan1120@naver.com>
2020-12-01 02:27:26 +0900

    Feat : MessageBox Model append 및 setMessageIsFirst 함수 구현 - 새로 들어온 메시지가 해당 일자의 첫번째 메시지인지 확인하는 setMessageIsFirst 함수 구현 - 받은 메시지를 추가 할 때 사용 할 append 함수 구현 (append 전 해당 일자의 첫번째 메시지인지 확인 후 추가)
    Issue #46

--- iOS/PapagoTalk/PapagoTalk/Chat/MessageBox.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/MessageBox.swift
@@ -9,4 +9,19 @@ import Foundation
 
 final class MessageBox {
     var messages = [Message]()
+    
+    func append(_ message: Message) {
+        guard let lastMessage = messages.last else {
+            messages.append(message)
+            return
+        }
+        messages.append(setMessageIsFirst(of: message, comparedBy: lastMessage))
+    }
+    
+    func setMessageIsFirst(of newMessage: Message, comparedBy lastMessage: Message) -> Message {
+        let isSameDate = Calendar.isSameDate(of: newMessage.timeStamp, with: lastMessage.timeStamp)
+        var message = newMessage
+        message.setIsFirst(with: isSameDate)
+        return message
+    }
 }
