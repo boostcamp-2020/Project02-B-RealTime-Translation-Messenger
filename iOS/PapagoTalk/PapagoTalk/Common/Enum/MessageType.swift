ce9b39c9358f1a7ad6f17a1b6c6a834a989dc56b
Minkwan Song <minkwan1120@naver.com>
2020-12-01 02:11:08 +0900

    Feat : MessageType Enum 생성 및 Codable 채택
    Issue #46

---
+++ iOS/PapagoTalk/PapagoTalk/Common/Enum/MessageType.swift
@@ -0,0 +1,13 @@
+//
+//  MessageType.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/11/30.
+//
+
+import Foundation
+
+enum MessageType: String, Codable {
+    case sent
+    case received
+}
