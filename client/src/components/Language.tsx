0c8f3b0962675d1998fe4df900578fada39dbc7e
Yihyun <hihyun16@gmail.com>
2020-12-02 00:41:44 +0900

    Feat : create Language component - 언어 선택 버튼 컴포넌트를 구현 - 추후 언어에 대한 state는 상위 컴포넌트에서 관리하도록 리팩토링 필요
    Issue #122

---
+++ client/src/components/Language.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const LanguageWrapper = styled.div`
+  text-align: center;
+`;
+
+const LanguageButton = styled.input<{ selected: boolean }>`
+  width: 50px;
+  height: 50px;
+  color: ${(props) =>
+    props.selected ? props.theme.blackColor : props.theme.whiteColor};
+  background-color: ${(props) =>
+    props.selected ? props.theme.lightGrayColor : props.theme.darkGrayColor};
+  border: ${(props) => props.theme.boxBorder};
+  border-radius: ${(props) => props.theme.borderRadius};
+  font-size: 1.2rem;
+  box-sizing: border-box;
+
+  &:hover {
+    color: ${(props) => props.theme.blackColor};
+    background-color: ${(props) => props.theme.lightGrayColor};
+  }
+`;
+
+const Language: React.FC = () => {
+  const [lang, setLang] = useState('ko');
+
+  return (
+    <LanguageWrapper>
+      <LanguageButton
+        type="button"
+        value="한"
+        selected={lang === 'ko'}
+        onClick={() => setLang('ko')}
+      />
+      <LanguageButton
+        type="button"
+        value="En"
+        selected={lang === 'en'}
+        onClick={() => setLang('en')}
+      />
+    </LanguageWrapper>
+  );
+};
+
+export default Language;
