89aa5bf74462ec1e9206da3ed6ea54ce4c6dbe9b
Minkwan Song <minkwan1120@naver.com>
2020-12-01 13:18:56 +0900

    Feat : ChatDrawerViewController reactor state Binding 및 configureChatDrawerUserCell() 함수 구현
    Issue #101

--- iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewController.swift
+++ iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewController.swift
@@ -16,7 +16,7 @@ final class ChatDrawerViewController: UIViewController, StoryboardView {
     @IBOutlet private weak var leaveChatRoomButton: UIButton!
     
     var disposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewSize()
@@ -41,10 +41,40 @@ final class ChatDrawerViewController: UIViewController, StoryboardView {
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
+        reactor.state.map { $0.users }
+            .asObservable()
+            .bind(to: userListCollectionView.rx.items) { [weak self] (_, row, element) in
+                guard let cell = self?.configureChatDrawerUserCell(at: row, with: element) else {
+                    return UICollectionViewCell()
+                }
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        // reactor.state.map { $0.roomCode }
+        
+        reactor.state.map { $0.leaveChatRoom }
+            .filter { $0 }
+            .do { [weak self] _ in
+                self?.dismiss(animated: true, completion: {
+                    self?.navigationController?.popViewController(animated: true)
+                })
+            }
+            .subscribe()
+            .disposed(by: disposeBag)
     }
     
     private func configureViewSize() {
         
+    }
+    
+    private func configureChatDrawerUserCell(at row: Int, with element: User) -> UICollectionViewCell {
+        guard let cell = userListCollectionView.dequeueReusableCell(withReuseIdentifier: ChatDrawerUserCell.identifier,
+                                                                    for: IndexPath(row: row, section: .zero)) as? ChatDrawerUserCell else {
+            return UICollectionViewCell()
+        }
+        cell.configureUserCell(with: element)
+        return cell
     }
     
     // view did load -> view 크기 계산 + Animation + 흐림 Effect
