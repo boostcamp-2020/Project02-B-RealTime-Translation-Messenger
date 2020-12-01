88390496ebceac983da222fef0280c6d0205ad05
Taegyung Gong <gtg92t@gmail.com>
2020-12-01 18:54:06 +0900

    Feat : Add button component - button component 추가 - text, color를 props로 받음 - default color 설정
    Issue #121

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Theme } from '../styles/Theme';
+
+interface Props {
+  text: string;
+  color?: string;
+}
+
+const { blueColor } = Theme;
+
+const Container = styled.button`
+  width: 100%;
+  padding: 15px 0px;
+  color: white;
+  border: 0;
+  background-color: ${(props) => props.color};
+  border-radius: ${(props) => props.theme.borderRadius};
+  font-size: 20px;
+  font-weight: 600;
+  text-align: center;
+`;
+
+const Button: FC<Props> = ({ text, color }) => (
+  <Container color={color}>{text}</Container>
+);
+
+Button.defaultProps = {
+  text: '',
+  color: blueColor,
+};
+
+export default Button;
