5bd2b0632ce8422be001b61770b4706fab6283b8
Yihyun <hihyun16@gmail.com>
2020-12-02 00:43:34 +0900

    Feat : create Footer component - 화면 하단을 구성하는 Footer 컴포넌트 구현
    Issue #123

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%);
+  bottom: 1.5rem;
+  color: ${(props) => props.theme.text};
+  text-align: center;
+  font-size: 1.2rem;
+  strong {
+    font-weight: bold;
+  }
+`;
+
+const Footer: React.FC = () => {
+  return (
+    <FooterWrapper>
+      Powered by
+      <strong> papago</strong>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
