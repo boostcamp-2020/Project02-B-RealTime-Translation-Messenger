a6236a0de6bab66f023746625b921f0407831bd2
ByoungHwi <1994blyo@gamil.com>
2020-12-01 13:40:55 +0900

    Chore : swiftlint.yml 수정 - unowned self 사용시 코드 간결화를 위한 unused_capture_list 제외
    Issue #116

---
+++ iOS/PapagoTalk/PapagoTalk/Coordinator/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/01.
+//
+
+import Foundation
---
+++ iOS/PapagoTalk/PapagoTalk/Coordinator/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/01.
+//
+
+import Foundation
