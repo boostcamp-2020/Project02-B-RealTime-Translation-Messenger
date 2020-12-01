03bda2309681e4fb691a7c1dad49e389b73eace9
Minkwan Song <minkwan1120@naver.com>
2020-12-01 11:10:04 +0900

    Feat : ChatDrawerViewController Reactor 생성 및 Reactor Action Binding
    Issue #101

--- iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewController.swift
+++ iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewController.swift
@@ -20,13 +20,33 @@ final class ChatDrawerViewController: UIViewController, StoryboardView {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewSize()
+        reactor = ChatDrawerViewReactor()
     }
     
     func bind(reactor: ChatDrawerViewReactor) {
+        self.rx.viewWillAppear
+            .map { _ in
+                Reactor.Action.fetchUsers
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        chatRoomCodeButton.rx.tap
+            .map { Reactor.Action.chatRoomCodeButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        leaveChatRoomButton.rx.tap
+            .map { Reactor.Action.leaveChatRoomButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
     }
     
     private func configureViewSize() {
         
     }
+    
+    // view did load -> view 크기 계산 + Animation + 흐림 Effect
+    // tap gesture, pan gesture
 }
