8c11b66779b7456ff441835941ed80b8b4313539
Minkwan Song <minkwan1120@naver.com>
2020-12-01 13:19:45 +0900

    Feat : ChatDrawerUserCell configureUserCell 함수 구현 - configureUserCell을 통해 Cell에 User image, nickName, language 표시 - configureImage() 함수 별도 함수로 구현 (imageURL URL 타입으로 변경 및 kingfisher를 이용한 캐싱 및 setImage)
    Issue #101

--- iOS/PapagoTalk/PapagoTalk/ChatDrawer/View/ChatDrawerUserCell.swift
+++ iOS/PapagoTalk/PapagoTalk/ChatDrawer/View/ChatDrawerUserCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class ChatDrawerUserCell: UICollectionViewCell {
     
@@ -13,4 +14,16 @@ final class ChatDrawerUserCell: UICollectionViewCell {
     @IBOutlet private weak var nickNameLabel: UILabel!
     @IBOutlet private weak var languageLabel: UILabel!
     
+    func configureUserCell(with user: User) {
+        configureImage(with: user.image)
+        nickNameLabel.text = user.nickName
+        languageLabel.text = user.language.localizedText
+    }
+    
+    private func configureImage(with imageURL: String) {
+        guard let imageURL = try? imageURL.asURL() else {
+            return
+        }
+        profileImageView.kf.setImage(with: imageURL)
+    }
 }
