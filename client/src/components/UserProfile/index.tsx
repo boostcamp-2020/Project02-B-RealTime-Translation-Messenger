731c77b772721f8de2553204cac829f946c5f24c
johnyejin <johnyejin@kakao.com>
2020-12-01 21:53:59 +0900

    Feat : random avatar 생성 구현 - random avatar url constant로 분리 - random avatar 생성 함수 utils로 분리 - input nickname change 이벤트 구현 Issue #127

--- client/src/components/UserProfile/index.tsx
+++ client/src/components/UserProfile/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import util from '../../utils/utils';
+import { avatar as Avatar } from '../../constants/avatar';
 import { Refresh } from '../Icons';
 import S from './style';
 
 const UserProfile: React.FC = () => {
+  const [avatar, setAvatar] = useState(Avatar[0]);
+  const [nickname, setNickname] = useState('');
+
+  useEffect(() => {}, [avatar]);
+
+  const onClickRefresh = () => {
+    const randomAvatar: string = util.getRandomAvatar();
+    setAvatar(randomAvatar);
+  };
+
+  const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+  };
+
   return (
     <S.ProfileWrapper>
       <S.AvatarWrapper>
-        <S.Avatar src="https://user-images.githubusercontent.com/26537048/100733385-72cc1d80-3411-11eb-9f23-e60f603d019a.png" />
-        <S.RefreshButton>
+        <S.Avatar src={avatar} />
+        <S.RefreshButton onClick={onClickRefresh}>
           <Refresh size={30} />
         </S.RefreshButton>
       </S.AvatarWrapper>
-      <S.NicknameInput placeholder="닉네임 입력" />
+      <S.NicknameInput placeholder="닉네임 입력" onChange={onChangeNickname} />
     </S.ProfileWrapper>
   );
 };
---
+++ client/src/constants/avatar.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+export const avatar: string[] = [
+  'https://user-images.githubusercontent.com/26537048/100733385-72cc1d80-3411-11eb-9f23-e60f603d019a.png', // 원본
+  'https://user-images.githubusercontent.com/26537048/100736204-c3457a00-3415-11eb-985d-754e079a283a.png', // 갈색
+  'https://user-images.githubusercontent.com/26537048/100736303-e40dcf80-3415-11eb-9f45-1f027f33f2c9.png', // 귤
+  'https://user-images.githubusercontent.com/26537048/100736398-0273cb00-3416-11eb-9cd4-24fbf73873d2.png', // 마젠타
+  'https://user-images.githubusercontent.com/26537048/100736422-13244100-3416-11eb-9103-6d33383acff1.png', // 모르겠다
+  'https://user-images.githubusercontent.com/26537048/100736479-27683e00-3416-11eb-87f2-8cb654bbf672.png', // 보라
+  'https://user-images.githubusercontent.com/26537048/100736528-3949e100-3416-11eb-8f13-4f7d56f920f2.png', // 부엉이
+  'https://user-images.githubusercontent.com/26537048/100736573-4bc41a80-3416-11eb-8935-49b7e163f4b3.png', // 비둘기
+  'https://user-images.githubusercontent.com/26537048/100736659-701ff700-3416-11eb-885b-174912711d35.png', // 에메랄드
+  'https://user-images.githubusercontent.com/26537048/100736753-8c239880-3416-11eb-8982-6a4736c90caf.png', // 민트
+  'https://user-images.githubusercontent.com/26537048/100736811-9f366880-3416-11eb-9bc9-5e37c81708b1.png', // 노랑
+  'https://user-images.githubusercontent.com/26537048/100736860-b2e1cf00-3416-11eb-91bf-5638db01548c.png', // 연두
+  'https://user-images.githubusercontent.com/26537048/100736907-c3924500-3416-11eb-9ae9-9cb30e68557f.png', // 자몽
+  'https://user-images.githubusercontent.com/26537048/100736940-d4db5180-3416-11eb-8fa2-daf19e362a06.png', // 코랄
+  'https://user-images.githubusercontent.com/26537048/100737018-f5a3a700-3416-11eb-9c37-0f3eacd488a6.png', // 찐초록
+  'https://user-images.githubusercontent.com/26537048/100737070-07854a00-3417-11eb-8015-69bda246a26d.png', // 초록
+  'https://user-images.githubusercontent.com/26537048/100737132-1a981a00-3417-11eb-88a8-ee5ac11a7222.png', // 탁한초록
+  'https://user-images.githubusercontent.com/26537048/100737176-2a176300-3417-11eb-854f-835bbe6c8363.png', // 탁한하늘
+  'https://user-images.githubusercontent.com/26537048/100737224-3a2f4280-3417-11eb-913c-ea0a90ce9d02.png', // 파스텔빨강
+  'https://user-images.githubusercontent.com/26537048/100737285-4ca97c00-3417-11eb-98d7-2ff98cae04d0.png', // 파스텔초록
+  'https://user-images.githubusercontent.com/26537048/100737333-5df28880-3417-11eb-9025-e711bf03361f.png', // 펭귄
+  'https://user-images.githubusercontent.com/26537048/100737382-7498df80-3417-11eb-8df2-1c2ca02da321.png', // 핑크
+  'https://user-images.githubusercontent.com/26537048/100737463-8ed2bd80-3417-11eb-9f7a-daa402ad59d3.png', // 하늘
+  'https://user-images.githubusercontent.com/26537048/100737533-a611ab00-3417-11eb-82f6-73253dd26bd9.png', // 형광연두
+];
