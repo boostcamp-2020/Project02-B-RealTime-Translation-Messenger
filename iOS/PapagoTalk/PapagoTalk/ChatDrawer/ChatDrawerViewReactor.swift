95835e7b838f30381ad787519429c1c7963ce493
Minkwan Song <minkwan1120@naver.com>
2020-12-02 10:53:18 +0900

    Feat : ChatDrawerViewReactor 네트워크 로직 및 채팅방 코드 복사 기능 구현
    Issue #100

--- iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewReactor.swift
+++ iOS/PapagoTalk/PapagoTalk/ChatDrawer/ChatDrawerViewReactor.swift
@@ -19,29 +19,41 @@ final class ChatDrawerViewReactor: Reactor {
     }
     
     enum Mutation {
-        case setUsers(UserList) // RoomNumber?
-        case alertRoomCode(String)
+        case setUsers(UserList)
+        case copyRoomCode(String)
+        case setNeedToast(Bool)
         case setLeaveChatRoom(Bool)
     }
     
     struct State {
         var users: UserList
-        var roomCode: String
+        var roomCode: String?
+        var needToast: Bool
         var leaveChatRoom: Bool
     }
     
+    private let networkService: NetworkServiceProviding
+    private let userData: UserDataProviding
+    private let roomID: Int
     let initialState: State
     
-    init() {
-        initialState = State(users: UserList(), roomCode: "", leaveChatRoom: false)
+    init(networkService: NetworkServiceProviding, userData: UserDataProviding, roomID: Int, roomCode: String) {
+        self.networkService = networkService
+        self.userData = userData
+        self.roomID = roomID
+        initialState = State(users: UserList(), roomCode: nil, needToast: false, leaveChatRoom: false)
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .fetchUsers:
-            return .just(Mutation.setUsers(UserList()))
+            return requestGetUserList(by: roomID)
         case .chatRoomCodeButtonTapped:
-            return .just(Mutation.alertRoomCode(""))
+            return .concat ([
+                .just(Mutation.setNeedToast(true)),
+                .just(Mutation.copyRoomCode("554305")),
+                .just(Mutation.setNeedToast(false))
+            ])
         case .leaveChatRoomButtonTapped:
             return .just(Mutation.setLeaveChatRoom(true))
         }
@@ -53,11 +65,24 @@ final class ChatDrawerViewReactor: Reactor {
         switch mutation {
         case .setUsers(let users):
             state.users = users
-        case .alertRoomCode(let roomCode):
+        case .copyRoomCode(let roomCode):
             state.roomCode = roomCode
+        case .setNeedToast(let needToast):
+            state.needToast = needToast
         case .setLeaveChatRoom(let leaveChatRoom):
             state.leaveChatRoom = leaveChatRoom
         }
         return state
+    }
+    
+    private func requestGetUserList(by roomID: Int) -> Observable<Mutation> {
+        return networkService.getUserList(of: roomID)
+            .asObservable()
+            .compactMap { $0.roomById?.users }
+            .map { $0.map { User(id: $0.id,
+                                 nickName: $0.nickname,
+                                 image: $0.avatar,
+                                 language: .codeToLanguage(of: $0.lang)) } }
+            .map { Mutation.setUsers($0) }
     }
 }
