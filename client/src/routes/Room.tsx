392546168e1eb73d815310dade880139db947a43
Taegyung Gong <gtg92t@gmail.com>
2020-12-02 22:51:20 +0900

    Feat : Update room page for confirm user information - user 정보를 확인하기 위해 room route 수정
    Issue #150

--- client/src/routes/Room.tsx
+++ client/src/routes/Room.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Room: React.FC = () => {
+  const location: any = useLocation();
+  const { userId, code, nickname, avatar, lang } = location.state;
   return (
     <>
       <h1>Room</h1>
+      <p>{userId}</p>
+      <p>{code}</p>
+      <p>{nickname}</p>
+      <p>{avatar}</p>
+      <p>{lang}</p>
     </>
   );
 };
