fb981a63ce5172de04d9b771fb81a50a47559f2c
Taegyung Gong <gtg92t@gmail.com>
2020-12-02 22:42:33 +0900

    Feat : Add room queries - 방 생성과 참여할 때 사용하는 mutation 추가
    Issue #152

---
+++ client/src/queries/room.queires.ts
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+export const ENTER_ROOM = gql`
+  mutation enterRoom(
+    $nickname: String!
+    $avatar: String!
+    $lang: String!
+    $code: String!
+  ) {
+    enterRoom(nickname: $nickname, avatar: $avatar, lang: $lang, code: $code) {
+      userId
+      roomId
+    }
+  }
+`;
+export const CREATE_ROOM = gql`
+  mutation createRoom($nickname: String!, $avatar: String!, $lang: String!) {
+    createRoom(nickname: $nickname, avatar: $avatar, lang: $lang) {
+      userId
+      roomId
+      code
+    }
+  }
+`;
