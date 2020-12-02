ad8bd9fd4d7079a472f8f8b6f2e76f6a37d0773c
Yihyun <hihyun16@gmail.com>
2020-12-02 10:52:49 +0900

    Refactor : LANGUAGE 상수화 - 언어 문자열을 상수화하여 관리
    Issue #123

--- client/src/components/Language.tsx
+++ client/src/components/Language.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import LANGUAGE from '../constants/language';
 
 const LanguageWrapper = styled.div`
   text-align: center;
@@ -23,21 +24,21 @@ const LanguageButton = styled.input<{ selected: boolean }>`
 `;
 
 const Language: React.FC = () => {
-  const [lang, setLang] = useState('ko');
+  const [lang, setLang] = useState(LANGUAGE.KO);
 
   return (
     <LanguageWrapper>
       <LanguageButton
         type="button"
         value="한"
-        selected={lang === 'ko'}
-        onClick={() => setLang('ko')}
+        selected={lang === LANGUAGE.KO}
+        onClick={() => setLang(LANGUAGE.KO)}
       />
       <LanguageButton
         type="button"
         value="En"
-        selected={lang === 'en'}
-        onClick={() => setLang('en')}
+        selected={lang === LANGUAGE.EN}
+        onClick={() => setLang(LANGUAGE.EN)}
       />
     </LanguageWrapper>
   );
