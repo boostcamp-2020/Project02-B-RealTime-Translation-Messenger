e71c3f91e9e1824fea7be61ac11a78b89494fdda
Minkwan Song <minkwan1120@naver.com>
2020-12-02 10:47:33 +0900

    Style : 코드 정리 및 String Extension asURL 코드 이동
    Issue #100

--- iOS/PapagoTalk/PapagoTalk/Common/Extension/String+.swift
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/String+.swift
@@ -13,6 +13,13 @@ extension String {
         return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: "")
     }
     
+    func asURL() throws -> URL {
+        guard let url = URL(string: self) else {
+            throw NetworkError.invalidURL
+        }
+        return url
+    }
+    
     func toDate() -> Date {
         return Date(timeIntervalSince1970: TimeInterval(self.toDouble()))
     }
