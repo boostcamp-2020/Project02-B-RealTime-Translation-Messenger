924c5bf222404beb9880827ac8d8bd350f72bc72
Taegyung Gong <gtg92t@gmail.com>
2020-12-02 18:00:23 +0900

    Chore : Apollo client provider 추가 - index.tsx에 apollo provider 추가
    Issue #152

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import App from './App';
+import Client from './apollo/client';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <ApolloProvider client={Client}>
+    <ApolloHooksProvider client={Client}>
+      <App />
+    </ApolloHooksProvider>
+  </ApolloProvider>,
+  rootElement,
+);
