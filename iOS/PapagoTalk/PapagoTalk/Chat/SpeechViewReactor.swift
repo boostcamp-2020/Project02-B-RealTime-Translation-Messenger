a30724c631b36f0fdf8def525ad5870ab6788d6a
Minkwan Song <minkwan1120@naver.com>
2020-12-02 21:20:29 +0900

    Feat : SpeechViewReactor Reactor 코드 틀 생성 - microphoneButtonTapped case 추가
    Issue #102

--- iOS/PapagoTalk/PapagoTalk/Chat/SpeechViewReactor.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/SpeechViewReactor.swift
@@ -10,5 +10,38 @@ import ReactorKit
 
 final class SpeechViewReactor: Reactor {
     
+    enum Action {
+        case microphoneButtonTapped
+    }
     
+    enum Mutation {
+        case mic
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState: State
+    
+    init() {
+        initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .microphoneButtonTapped:
+            return .just(Mutation.mic)
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        
+        switch mutation {
+        case .mic:
+            break
+        }
+        return state
+    }
 }
