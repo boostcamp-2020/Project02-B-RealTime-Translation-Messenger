51e36d9bb079d14c136dbd29183cbc7cf4452041
Minkwan Song <minkwan1120@naver.com>
2020-12-02 13:08:30 +0900

    Fix : Merge 후 파일 코드 충돌로 날아간 코드 복구

---
+++ iOS/PapagoTalk/PapagoTalk/Entity/TranslatedResult.swift
@@ -0,0 +1,13 @@
+//
+//  TranslatedResult.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/01.
+//
+
+import Foundation
+
+struct TranslatedResult: Codable {
+    var originText: String
+    var translatedText: String
+}
