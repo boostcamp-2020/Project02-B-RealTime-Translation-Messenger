d5338b00eb01a223fe5d050df7f95845e45190a1
Minkwan Song <minkwan1120@naver.com>
2020-12-03 20:40:54 +0900

    Feat : SpeechManager 음성인식 코드 분리
    Issue #171

--- iOS/PapagoTalk/PapagoTalk/Chat/SpeechManager.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/SpeechManager.swift
@@ -7,15 +7,16 @@
 
 import Foundation
 import Speech
+import RxSwift
 
 final class SpeechManager: NSObject {
     
-    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.autoupdatingCurrent)
+    private let speechRecognizer = SFSpeechRecognizer(locale: UserDataProvider().language.locale)
     private let audioEngine = AVAudioEngine()
     private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
     private var recognitionTask: SFSpeechRecognitionTask?
-    
-    var outputText: String?
+    private var speechToTextHandler: ((String) -> Void)?
+    var ob = BehaviorSubject<String>(value: "")
     
     override init() {
         super.init()
@@ -24,17 +25,14 @@ final class SpeechManager: NSObject {
     
     func speechToText() {
         guard audioEngine.isRunning else {
-            startSpeechRecognizing()
+//            speechToTextHandler = handler
+            DispatchQueue.main.async { [weak self] in
+                self?.startSpeechRecognizing()
+            }
             // microphoneButton.setTitle("stop", for: .normal)
             return
         }
         stopSpeechRecognizing()
-    }
-    
-    private func stopSpeechRecognizing() {
-        audioEngine.stop()
-        recognitionRequest?.endAudio()
-        // microphoneButton.setTitle("start", for: .normal)
     }
     
     private func startSpeechRecognizing() {
@@ -46,15 +44,25 @@ final class SpeechManager: NSObject {
         configureRecognitionRequest()
     }
     
+    private func stopSpeechRecognizing() {
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+        // microphoneButton.setTitle("start", for: .normal)
+    }
+    
     private func configureAVAudioSession() {
         let audioSession = AVAudioSession.sharedInstance()
         
-        do {
-            try audioSession.setCategory(AVAudioSession.Category.record)
-            try audioSession.setMode(AVAudioSession.Mode.measurement)
-            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-        } catch {
-            debugPrint(error.localizedDescription)
+        DispatchQueue.main.async {
+            do {
+                try audioSession.setCategory(AVAudioSession.Category.record)
+                try audioSession.setMode(AVAudioSession.Mode.measurement)
+                try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            } catch {
+                debugPrint(error.localizedDescription)
+                debugPrint("AVAudio")
+                // Toast(text: "음성인식 기능의 오류가 발생하였습니다.\n앱을 재시작 해주세요.", delay: 0, duration: 2).show()
+            }
         }
     }
     
@@ -64,6 +72,7 @@ final class SpeechManager: NSObject {
         
         guard let recognitionRequest = recognitionRequest else {
             debugPrint("SFSpeechAudioBufferRecognitionRequest create error")
+            // Toast(text: "음성인식 기능의 오류가 발생하였습니다.\n다시 시작해주세요.", delay: 0, duration: 2).show()
             return
         }
         recognitionRequest.shouldReportPartialResults = true
@@ -75,39 +84,46 @@ final class SpeechManager: NSObject {
         recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { [weak self] result, error in
             var isFinal = false
             
-            if result != nil {
-                self?.outputText = result?.bestTranscription.formattedString
-                isFinal = (result?.isFinal)!
+            if let result = result, let handler = self?.speechToTextHandler {
+                let output = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+                self?.ob.onNext(output)
             }
             
-            if error != nil || isFinal {
-                self?.audioEngine.stop()
-                inputNode.removeTap(onBus: .zero)
-                self?.recognitionRequest = nil
-                self?.recognitionTask = nil
-                // self?.microphoneButton.isEnabled = true
+            guard error != nil || isFinal else {
+                debugPrint("없음")
+                return
             }
+            self?.audioEngine.stop()
+            inputNode.removeTap(onBus: .zero)
+
+            self?.recognitionRequest = nil
+            self?.recognitionTask = nil
+            // self?.microphoneButton.isEnabled = true
         })
         configureAdditionalSpeechInput(on: inputNode)
     }
     
     private func configureAdditionalSpeechInput(on inputNode: AVAudioInputNode) {
         let recordingFormat = inputNode.outputFormat(forBus: .zero)
-        inputNode.installTap(onBus: .zero, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+        inputNode.installTap(onBus: .zero, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
             self.recognitionRequest?.append(buffer)
         }
-        
-        audioEngine.prepare()
-        do {
-            try audioEngine.start()
-        } catch {
-            debugPrint(error.localizedDescription)
+        DispatchQueue.main.async { [weak self] in
+            self?.audioEngine.prepare()
+            do {
+                try self?.audioEngine.start()
+            } catch {
+                debugPrint(error.localizedDescription)
+                debugPrint("마지막")
+            }
         }
     }
 }
 
 extension SpeechManager: SFSpeechRecognizerDelegate {
     func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        debugPrint("델리게이트 작동됨 ")
         guard available else {
             // microphoneButton.isEnabled = false
             return
