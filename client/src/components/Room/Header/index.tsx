05bfb978958e6f55461dba4b9d6dbe3ca2b452e5
johnyejin <johnyejin@kakao.com>
2020-12-03 11:34:53 +0900

    Style : 채팅방 Header html, css 구현
    Issue #158

---
+++ client/src/components/Room/Header/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Hamburger, Copy, Door } from '@components/Icons';
+import { AvatarStack, Avatar } from '@primer/components';
+import S from './style';
+
+const Header: React.FC = () => {
+  const history = useHistory();
+
+  return (
+    <S.Wrapper>
+      <S.HamburgerButton>
+        <Hamburger size={24} />
+      </S.HamburgerButton>
+      <S.CodeWrapper>
+        <S.Code>#601 235</S.Code>
+        <Copy size={20} />
+      </S.CodeWrapper>
+      <S.RightWrapper>
+        <AvatarStack alignRight>
+          <Avatar
+            style={{ width: '24px', height: '24px' }}
+            alt="Primer"
+            src="https://avatars.githubusercontent.com/primer"
+          />
+          <Avatar
+            style={{ width: '24px', height: '24px' }}
+            alt="GitHub"
+            src="https://avatars.githubusercontent.com/github"
+          />
+        </AvatarStack>
+        <S.DoorButton onClick={() => history.push('/')}>
+          <Door size={24} />
+        </S.DoorButton>
+      </S.RightWrapper>
+    </S.Wrapper>
+  );
+};
+
+export default Header;
