f1c623c77ba2861c7b17738564ac11e454f969ed
Minkwan Song <minkwan1120@naver.com>
2020-12-03 10:46:08 +0900

    Feat : Network Error case 추가
    Issue #99

--- iOS/PapagoTalk/PapagoTalk/Network/NetworkError.swift
+++ iOS/PapagoTalk/PapagoTalk/Network/NetworkError.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 enum NetworkError: Error {
+    
+    // MARK: Request Error
+    
+    case invalidToken
     case invalidURL
+    case requestFailure(message: String)
+
+    // MARK: Response Error
+    
+    case invalidResponse(message: String)
+    case invalidData(message: String)
+    
+    // MARK: Status Code
+    
+    case informational(message: String)
+    case redirection(message: String)
+    case clientError(message: String)
+    case serverError(message: String)
+
+    // MARK: Token
+
+    case tokenExpiration(message: String)
 }
