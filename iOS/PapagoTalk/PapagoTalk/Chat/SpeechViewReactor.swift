9e898f387d0fb099e10a7619321e595986094885
Minkwan Song <minkwan1120@naver.com>
2020-12-03 21:46:53 +0900

    Feat : SpeechViewReactor 음성 인식 모델 결과 binding 및 VC와 binding 구현
    Issue #171

--- iOS/PapagoTalk/PapagoTalk/Chat/SpeechViewReactor.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/SpeechViewReactor.swift
@@ -12,11 +12,11 @@ final class SpeechViewReactor: Reactor {
     
     enum Action {
         case microphoneButtonTapped
-        case speechTextChanged
+        case speechTextChanged(String)
     }
     
     enum Mutation {
-        case setSpeechRecognition
+        case setSpeechRecognition(String)
         case setTranslation
     }
     
@@ -32,22 +32,16 @@ final class SpeechViewReactor: Reactor {
     
     init() {
         initialState = State(speechRecognizedText: "", translatedText: "...")
-        speechManager.ob
-            .do {
-                print($0)
-            }
-            .map { _ in Action.speechTextChanged }
-            .bind(to: action)
-            .disposed(by: disposeBag)
+        bind()
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .microphoneButtonTapped:
             speechManager.speechToText()
-            return Observable.just(Mutation.setSpeechRecognition)
-        case .speechTextChanged:
-            return Observable.just(Mutation.setSpeechRecognition)
+            return Observable.just(Mutation.setSpeechRecognition(""))
+        case .speechTextChanged(let output):
+            return Observable.just(Mutation.setSpeechRecognition(output))
         }
     }
     
@@ -55,40 +49,18 @@ final class SpeechViewReactor: Reactor {
         var state = state
         
         switch mutation {
-        case .setSpeechRecognition:
-            state.speechRecognizedText = ""
+        case .setSpeechRecognition(let output):
+            state.speechRecognizedText = output
         case .setTranslation:
             state.translatedText = ""
         }
         return state
     }
     
-//    private func search(query: String?, page: Int) -> Observable<String> {
-//        let emptyResult: ([String], Int?) = ([], nil)
-//        guard let url = self.url(for: query, page: page) else { return .just(emptyResult) }
-//        return URLSession.shared.rx.json(url: url)
-//          .map { json -> ([String], Int?) in
-//            guard let dict = json as? [String: Any] else { return emptyResult }
-//            guard let items = dict["items"] as? [[String: Any]] else { return emptyResult }
-//            let repos = items.compactMap { $0["full_name"] as? String }
-//            let nextPage = repos.isEmpty ? nil : page + 1
-//            return (repos, nextPage)
-//          }
-//          .do(onError: { error in
-//            if case let .some(.httpRequestFailed(response, _)) = error as? RxCocoaURLError, response.statusCode == 403 {
-//              print("⚠️ GitHub API rate limit exceeded. Wait for 60 seconds and try again.")
-//            }
-//          })
-//          .catchErrorJustReturn(emptyResult)
-//      }
-//    }
+    private func bind() {
+        speechManager.recognizedSpeech
+            .map { Action.speechTextChanged($0) }
+            .bind(to: action)
+            .disposed(by: disposeBag)
+    }
 }
-
-//extension SpeechViewReactor.Action {
-//  static func isUpdateSpeech(_ action: SpeechViewReactor.Action) -> Bool {
-//    if case .microphoneButtonTapped = action {
-//      return true
-//    } else {
-//      return false
-//    }
-//  }
