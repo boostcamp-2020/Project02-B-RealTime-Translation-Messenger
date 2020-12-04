b51d572c9e8bad1585fa6e62a3cb3514fd3469e9
Taegyung Gong <gtg92t@gmail.com>
2020-12-04 10:53:41 +0900

    Feat : Use apollo ws for realtime chatting - apollo ws 이용 실시간 다자간 채팅 구현
    Issue #173

--- client/src/routes/Room.tsx
+++ client/src/routes/Room.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import RoomComponent from '@components/Room';
+import React, { FC, useEffect } from 'react';
+import { RouteComponentProps, useLocation } from 'react-router-dom';
+import { NEW_MESSAGE, ALL_MESSAGES_BY_ID } from '@/queries/room.queires';
+import { useQuery } from '@apollo/client';
+import ChatLog from '@components/ChatLog';
 
-const Room: React.FC = () => {
+interface MatchParams {
+  id: string;
+}
+
+const Room: FC<RouteComponentProps<MatchParams>> = ({ match }) => {
+  const roomId = +match.params.id;
   const location: any = useLocation();
-  const { userId, code, nickname, avatar, lang } = location.state;
+  const { lang } = location.state;
+  const { data, loading, subscribeToMore } = useQuery(ALL_MESSAGES_BY_ID, {
+    variables: {
+      id: roomId,
+    },
+  });
+
+  useEffect(() => {
+    const unsubscribe = subscribeToMore({
+      document: NEW_MESSAGE,
+      variables: { roomId, lang },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const { newMessage } = subscriptionData.data;
+        return {
+          allMessagesById: [...prev.allMessagesById, newMessage],
+        };
+      },
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  if (loading) return <div>Loading!</div>;
+
   return (
-    <>
-      <RoomComponent />
-      <h1>Room</h1>
-      <p>{userId}</p>
-      <p>{code}</p>
-      <p>{nickname}</p>
-      <p>{avatar}</p>
-      <p>{lang}</p>
-    </>
+    <div>
+      <div>New massages</div>
+      <ChatLog messages={data.allMessagesById} />
+    </div>
   );
 };
 
