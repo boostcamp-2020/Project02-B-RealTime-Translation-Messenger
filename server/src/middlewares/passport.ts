a0fc49fc45c4c62332a1cf79b7b1d168bb4ccd01
Taegyung Gong <gtg92t@gmail.com>
2020-12-06 16:52:03 +0900

    Feat : Add passport config setting - passport 설정 - req.user에 user의 id, profile, lang 그리고 속한 방 id를 넣어줌
    Issue #230

---
+++ server/src/middlewares/passport.ts
@@ -0,0 +1,46 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('Token'),
+  secretOrKey: process.env.JWT_SECRET_KEY,
+};
+
+const verifyJwt = async (payload: any, done: any) => {
+  try {
+    const isExist: any = await prisma.user.findOne({
+      where: {
+        id: payload.id,
+      },
+    });
+
+    const user = {
+      id: payload.id,
+      avatar: payload.avatar,
+      lang: payload.lang,
+      roomId: payload.roomId,
+    };
+
+    if (isExist) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (err) {
+    return done(err, false);
+  }
+};
+
+export const authenticateJwt = (req: any, res: any, next: any): any => {
+  passport.authenticate('jwt', { session: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+};
+
+passport.use(new JwtStrategy(jwtOptions, verifyJwt));
