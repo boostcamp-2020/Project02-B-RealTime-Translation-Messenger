a61efd7903788726c77a2ed1b9d035e4b6e6ff6d
Minkwan Song <minkwan1120@naver.com>
2020-12-08 23:56:20 +0900

    Test : ChatCodeInputReactor codeNumber Input tests - 첫번째 입력, 세번째 입력, 여섯 번째 입력 test
    Issue #298

---
+++ iOS/PapagoTalk/PapagoTalkTests/ChatCodeInputReactorTests.swift
@@ -0,0 +1,59 @@
+//
+//  ChatCodeInputReactorTests.swift
+//  PapagoTalkTests
+//
+//  Created by 송민관 on 2020/12/08.
+//
+
+import XCTest
+
+class ChatCodeInputReactorTests: XCTestCase {
+
+    func test_codeNumberInput_firstNumber() throws {
+        // Given
+        let reactor = ChatCodeInputViewReactor(networkService: ApolloNetworkServiceMockSuccess(),
+                                               userData: UserDataProviderMock())
+        
+        // When
+        reactor.action.onNext(.numberButtonTapped("5"))
+        
+        // Then
+        XCTAssertEqual(reactor.currentState.codeInput[0], "5")
+        XCTAssertEqual(reactor.currentState.cusor, 1)
+    }
+    
+    func test_codeNumberInput_thirdNumber() throws {
+        // Given
+        let reactor = ChatCodeInputViewReactor(networkService: ApolloNetworkServiceMockSuccess(),
+                                               userData: UserDataProviderMock())
+        reactor.action.onNext(.numberButtonTapped("5"))
+        reactor.action.onNext(.numberButtonTapped("4"))
+
+        // When
+        reactor.action.onNext(.numberButtonTapped("5"))
+
+        // Then
+        XCTAssertEqual(reactor.currentState.codeInput[2], "5")
+        XCTAssertEqual(reactor.currentState.cusor, 3)
+    }
+    
+    func test_codeNumberInput_sixthNumber() throws {
+        // Given
+        let reactor = ChatCodeInputViewReactor(networkService: ApolloNetworkServiceMockSuccess(),
+                                               userData: UserDataProviderMock())
+        reactor.action.onNext(.numberButtonTapped("8"))
+        reactor.action.onNext(.numberButtonTapped("9"))
+        reactor.action.onNext(.numberButtonTapped("0"))
+        reactor.action.onNext(.numberButtonTapped("1"))
+        reactor.action.onNext(.numberButtonTapped("2"))
+
+        // When
+        reactor.action.onNext(.numberButtonTapped("3"))
+
+        // Then
+        XCTAssertEqual(reactor.currentState.codeInput[5], "3")
+        XCTAssertEqual(reactor.currentState.cusor, 6)
+    }
+    
+    
+}
