6b54b6d3fb34c9a14c93a69f06a58e77e013a357
johnyejin <johnyejin@kakao.com>
2020-12-08 22:16:45 +0900

    Feat: useUsers hook 리턴값에 채팅방 입장,퇴장 유저 추가 - 바뀐 유저(입장, 퇴장)를 useUsers hook에서 useState로 관리 Issue #276

--- client/src/hooks/useUsers.ts
+++ client/src/hooks/useUsers.ts
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { NEW_USER, SUBSCRIBE_DELETE_USER } from '@/queries/user.queries';
+import { useState, useEffect } from 'react';
+import { NEW_USER, SUBSCRIBE_DELETE_USER } from '@queries/user.queries';
 import { useQuery } from '@apollo/client';
-import { User } from '../generated/types';
-import { ROOM_BY_ID } from '../queries/room.queires';
+import { User } from '@generated/types';
+import { ROOM_BY_ID } from '@queries/room.queires';
 
 interface Variables {
   roomId: number;
@@ -11,6 +11,12 @@ interface Variables {
 interface QueryReturnType {
   data: any;
   loading: boolean;
+  changeUser: ChangeUserType | undefined;
+}
+
+interface ChangeUserType {
+  type: string;
+  user: User;
 }
 
 const useUsers = ({ roomId }: Variables): QueryReturnType => {
@@ -19,6 +25,9 @@ const useUsers = ({ roomId }: Variables): QueryReturnType => {
       id: roomId,
     },
   });
+  const [changeUser, setChangeUser] = useState<ChangeUserType | undefined>(
+    undefined,
+  );
 
   useEffect(() => {
     const unsubscribe = subscribeToMore({
@@ -27,6 +36,7 @@ const useUsers = ({ roomId }: Variables): QueryReturnType => {
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData.data) return prev;
         const { newUser } = subscriptionData.data;
+        setChangeUser({ type: 'CREATE', user: newUser });
         return {
           ...prev,
           roomById: {
@@ -48,9 +58,12 @@ const useUsers = ({ roomId }: Variables): QueryReturnType => {
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData.data) return prev;
         const { deleteUser } = subscriptionData.data;
-        const newList = prev.roomById.users.filter(
-          (user: User) => user.id !== deleteUser.id,
-        );
+
+        const newList = prev.roomById.users.filter((user: User) => {
+          if (user.id !== deleteUser.id) return true;
+          setChangeUser({ type: 'DELETE', user });
+          return false;
+        });
         return {
           ...prev,
           roomById: {
@@ -65,7 +78,7 @@ const useUsers = ({ roomId }: Variables): QueryReturnType => {
     };
   }, []);
 
-  return { data, loading };
+  return { data, loading, changeUser };
 };
 
 export default useUsers;
