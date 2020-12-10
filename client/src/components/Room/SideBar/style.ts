e58ce24a9afa0f2b7e2593f4ecf8fff975a2b0fe
Taegyung Gong <gtg92t@gmail.com>
2020-12-11 01:36:15 +0900

    Feat : Add side bar animation - sidebar를 반응형으로 변경 media size에서 sidebar가 전체화면을   차지하도록 변경 - animation 추가
    Issue #375

--- client/src/components/Room/SideBar/style.ts
+++ client/src/components/Room/SideBar/style.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
+interface Props {
+  visible?: boolean;
+}
+
 export default {
-  SideBarWrapper: styled.div`
+  SideBarWrapper: styled.div<Props>`
     position: fixed;
     top: 0;
     left: 0;
@@ -14,6 +18,14 @@ export default {
     border-bottom-right-radius: 24px;
     font-size: 1.2rem;
     z-index: 2;
+    transform: ${({ visible }) =>
+      visible ? 'translateX(0)' : 'translateX(-100%)'};
+    transition: transform 0.3s ease-in-out;
+
+    @media (max-width: ${({ theme }) => theme.mediaSize}) {
+      width: 100%;
+      border-radius: 0;
+    }
   `,
   SideBarHeader: styled.div`
     display: flex;
@@ -26,7 +38,16 @@ export default {
       fill: ${(props) => props.theme.whiteColor};
     }
   `,
-  HamburgerButton: styled.button``,
+  HeaderText: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 5rem;
+    width: 100%;
+    margin-left: 3rem;
+    padding: 0.7rem 1rem 0rem 1rem;
+    font-weight: 700;
+  `,
   UserList: styled.ul`
     width: 100%;
   `,
