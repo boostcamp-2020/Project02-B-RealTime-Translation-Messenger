a38e95cb7a73d196536168412bb48216f02a8191
Minkwan Song <minkwan1120@naver.com>
2020-12-10 15:16:02 +0900

    Feat : User의 sent 메시지가 번역된 UserTranslatedMessageCell 생성 및 구현
    Issue #346

---
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell/TranslatedMessageCellByUser.swift
@@ -0,0 +1,19 @@
+//
+//  UserTranslatedMessageCell.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/12/10.
+//
+
+import UIKit
+
+final class UserTranslatedMessageCell: UICollectionViewCell {
+    
+}
+
+extension UserTranslatedMessageCell: MessageCell {
+    func configureMessageCell(message: Message) {
+        configureMessage(of: messageTextView, with: message.text)
+        configureTime(of: timeLabel, with: message.time, shouldShow: message.shouldTimeShow)
+    }
+}
---
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell/UserTranslatedMessageCell.swift
@@ -0,0 +1,8 @@
+//
+//  UserTranslatedMessageCell.swift
+//  PapagoTalk
+//
+//  Created by 송민관 on 2020/12/10.
+//
+
+import Foundation
