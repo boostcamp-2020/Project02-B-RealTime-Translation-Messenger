39f174d98aff5c63efb34cb760e89db6f8adf664
Minkwan Song <minkwan1120@naver.com>
2020-12-10 15:18:36 +0900

    Fix : UserTranslatedMessageCell 파일 reference 문제 해결
    Issue #346

--- iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell/UserTranslatedMessageCell.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell/UserTranslatedMessageCell.swift
@@ -5,4 +5,17 @@
 //  Created by 송민관 on 2020/12/10.
 //
 
-import Foundation
+import UIKit
+
+final class UserTranslatedMessageCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var messageTextView: MessageText!
+    @IBOutlet private weak var timeLabel: UILabel!
+}
+
+extension UserTranslatedMessageCell: MessageCell {
+    func configureMessageCell(message: Message) {
+        configureMessage(of: messageTextView, with: message.text)
+        configureTime(of: timeLabel, with: message.time, shouldShow: message.shouldTimeShow)
+    }
+}
