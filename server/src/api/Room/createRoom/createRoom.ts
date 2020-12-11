2ff6bc82233aefaa1ba51e65277a5f9648219204
Yihyun <hihyun16@gmail.com>
2020-12-11 09:58:10 +0900

    Refactor : delete publish event to create/enterRoom API - client에서의 lazy connect를 위해 publish logic 분리
    Issue #364

--- server/src/api/Room/createRoom/createRoom.ts
+++ server/src/api/Room/createRoom/createRoom.ts
@@ -15,7 +15,6 @@ export default {
     createRoom: async (
       _: any,
       args: User,
-      { pubsub }: any,
     ): Promise<{ userId: number; roomId: number; code: string; token: string }> => {
       const { nickname, avatar, lang } = args;
       const newUser = await prisma.user.create({
@@ -38,28 +37,6 @@ export default {
         },
       });
       const jwtToken = generateToken(newUser, newRoom.id);
-
-      const newMessage = await prisma.message.create({
-        data: {
-          text: `${nickname}님이 들어왔습니다`,
-          source: 'in',
-          user: {
-            connect: {
-              id: newUser.id,
-            },
-          },
-          room: {
-            connect: {
-              id: newRoom.id,
-            },
-          },
-        },
-        include: {
-          user: true,
-        },
-      });
-
-      pubsub.publish('NEW_MESSAGE', { newMessage });
 
       return { userId: newUser.id, roomId: newRoom.id, code: randomCode, token: jwtToken };
     },
--- server/src/api/Room/enterRoom/enterRoom.ts
+++ server/src/api/Room/enterRoom/enterRoom.ts
@@ -1,6 +1,5 @@
 import generateToken from '@utils/generateToken';
 import { PrismaClient } from '@prisma/client';
-import TRIGGER from '@utils/trigger';
 
 const prisma = new PrismaClient();
 
@@ -17,7 +16,6 @@ export default {
     enterRoom: async (
       _: any,
       { nickname, avatar, lang, code }: EnterInfo,
-      { pubsub }: any,
     ): Promise<{ userId: number; roomId: number; token: string }> => {
       const newUser = await prisma.user.create({
         data: {
@@ -31,30 +29,6 @@ export default {
         include: { rooms: true },
       });
       const jwtToken = generateToken(newUser, newUser.rooms[0].id);
-
-      const newMessage = await prisma.message.create({
-        data: {
-          text: `${nickname}님이 들어왔습니다`,
-          source: 'in',
-          user: {
-            connect: {
-              id: newUser.id,
-            },
-          },
-          room: {
-            connect: {
-              id: newUser.rooms[0].id,
-            },
-          },
-        },
-        include: {
-          user: true,
-        },
-      });
-
-      pubsub.publish(TRIGGER.NEW_MESSAGE, { newMessage });
-      pubsub.publish(TRIGGER.NEW_USER, { newUser });
-
       return { userId: newUser.id, roomId: newUser.rooms[0].id, token: jwtToken };
     },
   },
