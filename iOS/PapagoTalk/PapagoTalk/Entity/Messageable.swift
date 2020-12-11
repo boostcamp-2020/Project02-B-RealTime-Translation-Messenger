3b90c81b7e3e8ab6b904acae4599c22cabf98560
Minkwan Song <minkwan1120@naver.com>
2020-12-11 11:27:15 +0900

    Feat : Messageable user extension에 추가
    Issue #356

--- iOS/PapagoTalk/PapagoTalk/Entity/Messageable.swift
+++ iOS/PapagoTalk/PapagoTalk/Entity/Messageable.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol Messageable {
     var id: Int { get set }
     var text: String { get set }
+    var userData: Userable { get }
     var source: String { get set }
 }
 
@@ -17,7 +18,13 @@ typealias GetMessageData = GetMessageSubscription.Data.NewMessage
 typealias GetMissingMessageData = GetMessageByTimeQuery.Data.AllMessagesByTime
 
 extension GetMessageData: Messageable {
+    var userData: Userable {
+        return self.user
+    }
 }
 
 extension GetMissingMessageData: Messageable {
+    var userData: Userable {
+        return self.user
+    }
 }
