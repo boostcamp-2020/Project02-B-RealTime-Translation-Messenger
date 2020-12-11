466df6f4db1ae0446771e8e44aebec78486bf3a3
Yihyun <hihyun16@gmail.com>
2020-12-11 09:55:55 +0900

    Feat : add onConnect event when start server - Subscription API에서도 사용자 토큰 정보를 이용하고자 함 - 서버가 시작하고 웹소켓 연결이 발생하면 토큰 복호화를 수행
    Issue #364

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import './env';
 import { GraphQLServer, PubSub } from 'graphql-yoga';
 import { authenticateJwt } from './middlewares/passport';
@@ -5,19 +6,36 @@ import isAuthenticated from './middlewares/isAuthenticated';
 import schema from './schema';
 import logger from 'morgan';
 import 'module-alias/register';
+import { verify } from 'jsonwebtoken';
 
 const PORT = process.env.PORT || 4000;
+
+const prisma = new PrismaClient();
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request, isAuthenticated, pubsub }),
+  context: ({ request, connection }) => ({ request, connection, isAuthenticated, pubsub }),
 });
 
 server.express.use(logger('dev'));
 server.express.use(authenticateJwt);
 
-server.start({ port: PORT, cors: { origin: '*' } }, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+server.start(
+  {
+    port: PORT,
+    cors: { origin: true },
+    subscriptions: {
+      onConnect: async (connectionParams: any, webSocket: any) => {
+        const user: any = verify(connectionParams, process.env.JWT_SECRET_KEY as string);
+        const findUser = await prisma.user.findOne({ where: { id: user.id } });
+        if (!findUser) throw new Error('Not valid user token');
+        return { user: { ...findUser, roomId: user.roomId } };
+      },
+    },
+  },
+  () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  },
+);
