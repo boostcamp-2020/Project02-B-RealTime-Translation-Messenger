12e31aeada6c3301533adbd97c98e6f1e05bf1e3
ByoungHwi <1994blyo@gamil.com>
2020-12-13 00:27:37 +0900

    Feat : HistoryView 파일 생성 및 IBOutlet연결 - ViewController 및 Cell 파일 생성 및 IBOutlet연결
    Issue #392

---
+++ iOS/PapagoTalk/PapagoTalk/History/HistoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryCell.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/13.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var reEnterButton: UIButton!
+    
+}
---
+++ iOS/PapagoTalk/PapagoTalk/History/HistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryViewController.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/13.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    @IBOutlet weak var historyTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
