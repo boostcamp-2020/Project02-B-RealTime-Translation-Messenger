4e90a2431915894b9e6475651a812971e3f36477
ByoungHwi <1994blyo@gamil.com>
2020-12-13 14:12:41 +0900

    Feat : History 관리를 위한 HistoryManager 구현 - PersistenceManager를 활용하여 코어데이터에 값을 저장하고 가져오는 매니저 구현
    Issue #392

---
+++ iOS/PapagoTalk/PapagoTalk/DataLayer/HistoryManager.swift
@@ -0,0 +1,51 @@
+//
+//  HistoryManager.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/13.
+//
+
+import CoreData
+
+struct HistoryManager {
+    
+    let persistenceManager: PersistenceManager
+    
+    func fetch() -> [ChatRoomHistory] {
+        let request: NSFetchRequest<HistoryInfo> = HistoryInfo.fetchRequest()
+        let result = persistenceManager.fetch(request: request)
+        
+        return result.map {
+            ChatRoomHistory(roomID: Int($0.roomID),
+                            code: $0.code ?? "",
+                            title: $0.title ?? "",
+                            usedNickname: $0.usedNickname ?? "",
+                            usedLanguage: Language.codeToLanguage(of: $0.usedLanguage ?? "ko"),
+                            usedImage: $0.usedImage ?? "",
+                            enterDate: $0.enterDate ?? Date())
+        }
+    }
+    
+    func insert(of history: ChatRoomHistory) {
+        let nsEntity = NSEntityDescription.entity(forEntityName: "HistoryInfo", in: persistenceManager.context)
+        
+        guard let entity = nsEntity else {
+            return
+        }
+        
+        let managedObject = NSManagedObject(entity: entity, insertInto: persistenceManager.context)
+        managedObject.setValue(history.roomID, forKey: "roomID")
+        managedObject.setValue(history.code, forKey: "code")
+        managedObject.setValue(history.title, forKey: "title")
+        managedObject.setValue(history.usedNickname, forKey: "usedNickname")
+        managedObject.setValue(history.usedLanguage.code, forKey: "usedLanguage")
+        managedObject.setValue(history.usedImage, forKey: "usedImage")
+        managedObject.setValue(history.enterDate, forKey: "enterDate")
+        
+        do {
+            try persistenceManager.context.save()
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+    }
+}
