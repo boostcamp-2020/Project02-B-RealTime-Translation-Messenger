a2743499bf0c87eed0dfb5d103bd3b976c0ab0e0
Minkwan Song <minkwan1120@naver.com>
2020-12-14 06:24:42 +0900

    Feat : MicButtonSize Enum index, description 프로퍼티 및 indexToType static 함수 추가
    Issue #359

--- iOS/PapagoTalk/PapagoTalk/Common/Enum/MicButtonSize.swift
+++ iOS/PapagoTalk/PapagoTalk/Common/Enum/MicButtonSize.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-enum MicButtonSize: Int, Codable {
+enum MicButtonSize: Int, Codable, CaseIterable {
     case big
     case medium
     case small
@@ -23,6 +23,47 @@ enum MicButtonSize: Int, Codable {
             return 50
         case .none:
             return 0
+        }
+    }
+    
+    var index: Int {
+        switch self {
+        case .big:
+            return 0
+        case .medium:
+            return 1
+        case .small:
+            return 2
+        case .none:
+            return 3
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .big:
+            return "Big".localized
+        case .medium:
+            return "Medium".localized
+        case .small:
+            return "Small".localized
+        case .none:
+            return "None".localized
+        }
+    }
+    
+    static func indexToType(of index: Int) -> MicButtonSize {
+        switch index {
+        case 0:
+            return .big
+        case 1:
+            return .medium
+        case 2:
+            return .small
+        case 3:
+            return .none
+        default:
+            return .none
         }
     }
 }
