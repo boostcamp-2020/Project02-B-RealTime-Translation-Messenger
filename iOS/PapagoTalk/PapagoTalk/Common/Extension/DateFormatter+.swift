3c8ffffb0803ad4cc4d245b886168fa7c62d851f
Minkwan Song <minkwan1120@naver.com>
2020-12-14 17:08:19 +0900

    Fix : DateFormatter extension chatHistoryTimeFormat yesterday format 적용 제외
    Issue #413

--- iOS/PapagoTalk/PapagoTalk/Common/Extension/DateFormatter+.swift
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/DateFormatter+.swift
@@ -24,6 +24,9 @@ extension DateFormatter {
     }
     
     static func chatHistoryTimeFormat(of date: Date, type: TimeFormatType) -> String {
+        guard type != .yesterday else {
+            return type.format
+        }
         dateFormatter.dateFormat = type.format
         dateFormatter.locale = Locale(identifier: Locale.autoupdatingCurrent.identifier)
         return dateFormatter.string(from: date)
