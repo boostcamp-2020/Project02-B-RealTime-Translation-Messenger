6ea0e2f71fd507629b3d7518415f3bf0ed1097dd
Minkwan Song <minkwan1120@naver.com>
2020-12-14 16:33:06 +0900

    Feat : Calendar extension 함수 추가 - isToday, isYesterdya, isSameYear 함수 추가
    Issue #413

--- iOS/PapagoTalk/PapagoTalk/Common/Extension/Calendar+.swift
+++ iOS/PapagoTalk/PapagoTalk/Common/Extension/Calendar+.swift
@@ -14,4 +14,16 @@ extension Calendar {
     static func isSameDate(of firstDate: Date, with secondDate: Date) -> Bool {
         return calendar.isDate(firstDate, inSameDayAs: secondDate)
     }
+    
+    static func isToday(of date: Date) -> Bool {
+        return calendar.isDateInToday(date)
+    }
+    
+    static func isYesterday(of date: Date) -> Bool {
+        return calendar.isDateInYesterday(date)
+    }
+    
+    static func isSameYear(of date: Date) -> Bool {
+        return calendar.isDate(Date(), equalTo: date, toGranularity: self.Component.year)
+    }
 }
