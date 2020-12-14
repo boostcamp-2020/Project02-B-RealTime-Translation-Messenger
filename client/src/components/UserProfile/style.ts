22cf6aa22849b4134fb6e5d88f7d150bcdfeef8d
johnyejin <johnyejin@kakao.com>
2020-12-15 01:39:00 +0900

    Style : 언어 선택 버튼 style - 기존의 한/영 버튼 삭제 - 언어 refresh 버튼 추가 Issue #409

--- client/src/components/UserProfile/style.ts
+++ client/src/components/UserProfile/style.ts
@@ -10,9 +10,8 @@ export default {
     position: relative;
     width: 14.5rem;
     height: 14.5rem;
-
-    background: ${(props) => props.theme.whiteColor};
-    border: ${(props) => props.theme.boxBorder};
+    background: ${({ theme }) => theme.whiteColor};
+    border: ${({ theme }) => theme.boxBorder};
     border-radius: 50%;
   `,
   Avatar: styled.img`
@@ -25,55 +24,63 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-
     bottom: 0.8rem;
     right: 0.8rem;
     padding: 0.5rem;
-
-    background: ${(props) => props.theme.grayColor};
+    background: ${({ theme }) => theme.grayColor};
     border-radius: 50%;
-
     svg {
-      fill: ${(props) => props.theme.whiteColor};
+      fill: ${({ theme }) => theme.whiteColor};
     }
   `,
   NicknameInput: styled.input`
     width: 10rem;
     margin: 1rem 0;
     padding: 0.5rem;
-
-    color: ${(props) => props.theme.text};
-    border-bottom: ${(props) => props.theme.boxBorder};
-
+    color: ${({ theme }) => theme.text};
+    border-bottom: ${({ theme }) => theme.boxBorder};
     font-size: 18px;
     text-align: center;
   `,
   LanguageTitle: styled.div`
     margin: 0 0 1rem 0;
-    color: ${(props) => props.theme.darkGrayColor};
+    color: ${({ theme }) => theme.darkGrayColor};
     font-size: 18px;
   `,
-
   LanguageWrapper: styled.div`
     margin: 1rem 0;
     text-align: center;
   `,
-
-  LanguageButton: styled.input<{ selected: boolean }>`
+  LanguageButtonWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  LanguageButton: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 50px;
     height: 50px;
     margin: 0 0.3rem;
-    color: ${(props) =>
-      props.selected ? props.theme.blackColor : props.theme.whiteColor};
-    background-color: ${(props) =>
-      props.selected ? props.theme.lightGrayColor : props.theme.darkGrayColor};
-    border-radius: ${(props) => props.theme.borderRadius};
+    background-color: ${({ theme }) => theme.lightGrayColor};
+    border-radius: ${({ theme }) => theme.borderRadius};
     font-size: 1.2rem;
-    font-weight: ${(props) => (props.selected ? 'bold' : '')};
-
+  `,
+  LanguageRefreshButton: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    margin: 0 0.3rem;
+    background-color: ${({ theme }) => theme.darkGrayColor};
+    border-radius: ${({ theme }) => theme.borderRadius};
     &:hover {
-      color: ${(props) => props.theme.blackColor};
-      background-color: ${(props) => props.theme.lightGrayColor};
+      filter: brightness(85%);
+    }
+    svg {
+      fill: ${({ theme }) => theme.whiteColor};
     }
   `,
 };
