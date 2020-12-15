2822db85989b507c0a3f9ef8dfc5e3e57a5f9684
Yihyun <hihyun16@gmail.com>
2020-12-15 19:07:52 +0900

    Refactor: Specify type of middlewares - 미들웨어들의 타입 명시
    Issue #468

--- server/src/middlewares/isAuthenticated.ts
+++ server/src/middlewares/isAuthenticated.ts
@@ -1,4 +1,6 @@
-export default (req: any): any => {
+import { RequestWithUser } from '@interfaces/request';
+
+export default (req: RequestWithUser): void => {
   if (!req.user) {
     throw Error('This user is a non-existent user.');
   }
