987469e53d81940c2029718a298b845356fff08a
Minkwan Song <minkwan1120@naver.com>
2020-12-15 14:21:26 +0900

    Feat : SystemMessageCell dateBadge IBOutlet 선언 및 configureDate 추가
    Issue #437

--- iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell/SystemMessageCell.swift
+++ iOS/PapagoTalk/PapagoTalk/Chat/View/MessageCell/SystemMessageCell.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class SystemMessageCell: UICollectionViewCell {
-    @IBOutlet weak var systemMessageBadge: UIButton!
+    
+    @IBOutlet private weak var dateBadge: UIButton!
+    @IBOutlet private weak var systemMessageBadge: UIButton!
+    @IBOutlet private weak var dateBadgeHeight: NSLayoutConstraint!
 }
 
 extension SystemMessageCell: MessageCell {
     func configureMessageCell(message: Message) {
+        configureDate(of: dateBadge, dateBadgeHeight: dateBadgeHeight, with: message.time, isFirst: message.isFirstOfDay)
         systemMessageBadge.setTitle(message.text, for: .normal)
     }
 }
