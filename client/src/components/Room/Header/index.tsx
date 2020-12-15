2e275a4d0ee4b8ac5924fd6beff21d49bc681f10
johnyejin <johnyejin@kakao.com>
2020-12-15 18:22:57 +0900

    Refactor : 코드 복사 토스트 메세지 부분 리팩토링
    Issue #448

--- client/src/components/Room/Header/index.tsx
+++ client/src/components/Room/Header/index.tsx
@@ -5,9 +5,11 @@ import { Copy, Door } from '@components/Icons';
 import { AvatarStack, Avatar } from '@primer/components';
 import { useMutation } from '@apollo/client';
 import { DELETE_USER } from '@queries/user.queries';
+import { CREATE_SYSTEM_MESSAGE } from '@queries/messege.queries';
 import { User } from '@generated/types';
 import client, { wsClient } from '@/apollo/Client';
-import { CREATE_SYSTEM_MESSAGE } from '@/queries/messege.queries';
+import floatToast from '@utils/toast';
+import TS from '@styles/toast';
 import S from './style';
 
 interface Props {
@@ -35,15 +37,7 @@ const Header: React.FC<Props> = ({ visible, setVisible, code, users }) => {
   };
 
   const toast = () => {
-    const toastMsg: Element = document.querySelector('.toast')!;
-
-    if (!toastMsg.classList.contains('reveal')) {
-      setTimeout(() => {
-        document.querySelector('.toast')!.classList.remove('reveal');
-      }, 1000);
-    }
-
-    toastMsg.classList.add('reveal');
+    floatToast('.copy-toast');
   };
 
   const onClickHamburger = () => {
@@ -51,42 +45,46 @@ const Header: React.FC<Props> = ({ visible, setVisible, code, users }) => {
   };
 
   return (
-    <S.Wrapper>
-      <S.HamburgerWrapper>
-        <S.HamburgerButton visible={visible} onClick={onClickHamburger}>
-          <div />
-          <div />
-          <div />
-        </S.HamburgerButton>
-      </S.HamburgerWrapper>
-      <CopyToClipboard text={code} onCopy={toast}>
-        <S.CodeWrapper>
-          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-          <S.Code>#{code}</S.Code>
-          <Copy size={20} />
-        </S.CodeWrapper>
-      </CopyToClipboard>
-      <S.RightWrapper>
-        <AvatarStack alignRight>
-          {users.map((user) => (
-            <Avatar
-              key={user.id}
-              style={{
-                width: '24px',
-                height: '24px',
-                backgroundColor: `${(props: any) => props.theme.whiteColor}`,
-              }}
-              alt={user.nickname}
-              src={user.avatar}
-            />
-          ))}
-        </AvatarStack>
-        <S.DoorButton onClick={leaveRoom}>
-          <Door size={24} />
-        </S.DoorButton>
-      </S.RightWrapper>
-      <S.Toast className="toast">코드가 복사되었습니다!</S.Toast>
-    </S.Wrapper>
+    <TS.ToastWrapper>
+      <S.Wrapper>
+        <S.HamburgerWrapper>
+          <S.HamburgerButton visible={visible} onClick={onClickHamburger}>
+            <div />
+            <div />
+            <div />
+          </S.HamburgerButton>
+        </S.HamburgerWrapper>
+        <CopyToClipboard text={code} onCopy={toast}>
+          <S.CodeWrapper>
+            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+            <S.Code>#{code}</S.Code>
+            <Copy size={20} />
+          </S.CodeWrapper>
+        </CopyToClipboard>
+        <S.RightWrapper>
+          <AvatarStack alignRight>
+            {users.map((user) => (
+              <Avatar
+                key={user.id}
+                style={{
+                  width: '24px',
+                  height: '24px',
+                  backgroundColor: `${(props: any) => props.theme.whiteColor}`,
+                }}
+                alt={user.nickname}
+                src={user.avatar}
+              />
+            ))}
+          </AvatarStack>
+          <S.DoorButton onClick={leaveRoom}>
+            <Door size={24} />
+          </S.DoorButton>
+        </S.RightWrapper>
+        <TS.Toast className="copy-toast" isTop>
+          코드가 복사되었습니다!
+        </TS.Toast>
+      </S.Wrapper>
+    </TS.ToastWrapper>
   );
 };
 
--- client/src/components/Room/Header/style.ts
+++ client/src/components/Room/Header/style.ts
@@ -18,12 +18,6 @@ export default {
     border-bottom: ${({ theme }) => theme.boxBorder};
     z-index: 1;
 
-    .reveal {
-      opacity: 1;
-      visibility: visible;
-      transform: translate(-50%, 0);
-    }
-
     ${({ theme }) =>
       !theme.isLight &&
       css`
@@ -108,20 +102,5 @@ export default {
     @media (max-width: ${({ theme }) => theme.mediaSize}) {
       margin-left: 0.3rem;
     }
-  `,
-  Toast: styled.div`
-    position: fixed;
-    top: 5.5rem;
-    left: 50%;
-    padding: 1rem 1.5rem;
-    color: #fff;
-    background: rgba(0, 0, 0, 0.5);
-    border-radius: 30px;
-    font-size: 0.8rem;
-    transform: translate(-50%, 10px);
-    transition: opacity 0.5s, visibility 0.5s, transform 0.5s;
-    opacity: 0;
-    visibility: hidden;
-    z-index: 10000;
   `,
 };
