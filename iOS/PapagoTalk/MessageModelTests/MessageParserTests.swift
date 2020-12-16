353e5a6d179c08becb0968b3c768e99c5e3616e2
ByoungHwi <1994blyo@gamil.com>
2020-12-16 17:14:24 +0900

    Test : 보낸메시지 Parsing Test - 사용자가 보낸 메시지 Parsing 테스트
    Issue #480

--- iOS/PapagoTalk/MessageModelTests/MessageParserTests.swift
+++ iOS/PapagoTalk/MessageModelTests/MessageParserTests.swift
@@ -57,27 +57,81 @@ class MessageParserTests: XCTestCase {
         XCTAssertEqual(parsedMessage[0].type, .system)
     }
     
-        func test_parser_should_not_parse_system_out_message() throws {
-            // Given
-            let receiver = MockReceiver(userID: 1,
-                                        language: .korean,
-                                        sameLanguageTranslation: true)
-            let messageParser = MessageParser(userData: receiver)
-            
-            let message = MockMessageFactory().message(messageID: 1, senderID: 2, source: "out")
-            // When
-            let parsedMessage = messageParser.parse(newMessage: message)
-            // Then
-            XCTAssertEqual(parsedMessage.count, 1)
-            XCTAssertEqual(parsedMessage[0].type, .system)
-        }
+    func test_parser_should_not_parse_system_out_message() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: true)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 2, source: "out")
+        // When
+        let parsedMessage = messageParser.parse(newMessage: message)
+        // Then
+        XCTAssertEqual(parsedMessage.count, 1)
+        XCTAssertEqual(parsedMessage[0].type, .system)
+    }
     
-//    func test_parser_should_not_parse_system_message() throws {
-//        // Given
-//
-//        // When
-//
-//        // Then
-//    }
-
+    func test_parse_sent_message_when_language_different() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: true)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 1, source: "ko")
+        // When
+        let parsedMessage = messageParser.parse(newMessage: message)
+        // Then
+        XCTAssertEqual(parsedMessage.count, 2)
+        XCTAssertEqual(parsedMessage[0].text, "OriginText")
+        XCTAssertEqual(parsedMessage[1].text, "TranslatedText")
+    }
+    
+    func test_parse_sent_message_when_language_same() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: true)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 1, source: "en")
+        // When
+        let parsedMessage = messageParser.parse(newMessage: message)
+        // Then
+        XCTAssertEqual(parsedMessage.count, 2)
+        XCTAssertEqual(parsedMessage[0].text, "OriginText")
+        XCTAssertEqual(parsedMessage[1].text, "TranslatedText")
+    }
+    
+    func test_parser_should_not_parse_same_language_when_flag_false() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: false)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 1, source: "ko")
+        // When
+        let parsedMessage = messageParser.parse(newMessage: message)
+        // Then
+        XCTAssertEqual(parsedMessage.count, 1)
+        XCTAssertEqual(parsedMessage[0].text, "OriginText")
+    }
+    
+    func test_parser_should_parse_same_language_when_flag_true() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: true)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 1, source: "ko")
+        // When
+        let parsedMessage = messageParser.parse(newMessage: message)
+        // Then
+        XCTAssertEqual(parsedMessage.count, 2)
+        XCTAssertEqual(parsedMessage[0].text, "OriginText")
+        XCTAssertEqual(parsedMessage[1].text, "TranslatedText")
+    }
 }
