b827e3647637a562b0c9151fb5135e712338510e
ByoungHwi <1994blyo@gamil.com>
2020-12-16 17:02:00 +0900

    Test : MessageParser 메시지 파싱 테스트 - MessageParser의 파싱 테스트
    Issue #480

--- iOS/PapagoTalk/MessageModelTests/MessageParserTests.swift
+++ iOS/PapagoTalk/MessageModelTests/MessageParserTests.swift
@@ -8,5 +8,38 @@
 import XCTest
 
 class MessageParserTests: XCTestCase {
+    func test_originTextParsing() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: true)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 2, source: "en")
+        
+        // When
+        
+        let originText = messageParser.parse(newMessage: message).first?.text
+        
+        // Then
+        XCTAssertEqual(originText, "OriginText")
+    }
+    
+    func test_translatedTextParsing() throws {
+        // Given
+        let receiver = MockReceiver(userID: 1,
+                                    language: .korean,
+                                    sameLanguageTranslation: true)
+        let messageParser = MessageParser(userData: receiver)
+        
+        let message = MockMessageFactory().message(messageID: 1, senderID: 2, source: "en")
+        
+        // When
+        
+        let translatedText = messageParser.parse(newMessage: message).last?.text
+        
+        // Then
+        XCTAssertEqual(translatedText, "TranslatedText")
+    }
 
 }
