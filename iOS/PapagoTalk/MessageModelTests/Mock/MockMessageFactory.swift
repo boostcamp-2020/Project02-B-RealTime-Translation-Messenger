d85231b60af95e13f993169160eefbb75b15c0a2
Minkwan Song <minkwan1120@naver.com>
2020-12-17 14:42:15 +0900

    Style : MockMessageFactory 코드 정리
    Issue #481

--- iOS/PapagoTalk/MessageModelTests/Mock/MockMessageFactory.swift
+++ iOS/PapagoTalk/MessageModelTests/Mock/MockMessageFactory.swift
@@ -9,25 +9,23 @@ import Foundation
 
 struct MockMessageFactory {
     
-    func message(messageID: Int, senderID: Int, source: String) -> MockMessage {
-        MockMessage(id: messageID,
-                    text: "{\"originText\":\"OriginText\",\"translatedText\":\"TranslatedText\"}",
-                    userData: MockUserData(id: senderID,
-                                           nickname: "nickname",
-                                           avatar: "avatar",
-                                           lang: "lang"),
-                    source: source,
-                    createdAt: "")
+    func message(messageID: Int, senderID: Int, source: String) -> MockMessageData {
+        let mockUserData = MockUserData(id: senderID, nickname: "nickname", avatar: "avatar", lang: "lang")
+        
+        return MockMessageData(id: messageID,
+                               text: "{\"originText\":\"OriginText\",\"translatedText\":\"TranslatedText\"}",
+                               userData: mockUserData,
+                               source: source,
+                               createdAt: "")
     }
     
-    func translationFailedMessage(messageID: Int, senderID: Int, source: String) -> MockMessage {
-        MockMessage(id: messageID,
-                    text: "{\"originText\":\"OriginText\",\"translatedText\":\"\"}",
-                    userData: MockUserData(id: senderID,
-                                           nickname: "nickname",
-                                           avatar: "avatar",
-                                           lang: "lang"),
-                    source: source,
-                    createdAt: "")
+    func translationFailedMessage(messageID: Int, senderID: Int, source: String) -> MockMessageData {
+        let mockUserData = MockUserData(id: senderID, nickname: "nickname", avatar: "avatar", lang: "lang")
+        
+        return MockMessageData(id: messageID,
+                               text: "{\"originText\":\"OriginText\",\"translatedText\":\"\"}",
+                               userData: mockUserData,
+                               source: source,
+                               createdAt: "")
     }
 }
