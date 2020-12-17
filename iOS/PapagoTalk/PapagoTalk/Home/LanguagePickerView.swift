7efe066f6881eeb1f7956215355dfc4f7e906f95
ByoungHwi <1994blyo@gamil.com>
2020-12-17 11:39:21 +0900

    Refactor : LanguagePickerView 커스텀 클래스로 분리 - LanguagePickerView를 커스텀 클래스로 분리
    Issue #493

---
+++ iOS/PapagoTalk/PapagoTalk/Home/LanguagePickerView.swift
@@ -0,0 +1,31 @@
+//
+//  LanguagePickerView.swift
+//  PapagoTalk
+//
+//  Created by Byoung-Hwi Yoon on 2020/12/17.
+//
+
+import UIKit
+
+final class LanguagePickerView: UIPickerView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        guard subviews.count > 1 else {
+            return
+        }
+        subviews[1].backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 1, alpha: 0)
+        subviews[1].layer.borderWidth = 2
+        subviews[1].layer.borderColor = UIColor(named: "PapagoBlue")?.cgColor
+    }
+    
+}
