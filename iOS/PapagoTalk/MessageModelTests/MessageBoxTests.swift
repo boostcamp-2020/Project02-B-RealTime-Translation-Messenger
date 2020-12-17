c0c9ab85966d748525e6e971a1e46ef1285d7e6e
Minkwan Song <minkwan1120@naver.com>
2020-12-17 14:34:04 +0900

    Test : MessageBoxTests append messages, first message test
    Issue #481

--- iOS/PapagoTalk/MessageModelTests/MessageBoxTests.swift
+++ iOS/PapagoTalk/MessageModelTests/MessageBoxTests.swift
@@ -8,5 +8,36 @@
 import XCTest
 
 class MessageBoxTests: XCTestCase {
+    
+    func test_append_messages() throws {
+        // Given
+        let messageBox = MessageBox()
+        let messages = [
+            Message(of: "test1", by: User()),
+            Message(of: "test2", by: User()),
+            Message(of: "test3", by: User()),
+            Message(of: "test4", by: User()),
+            Message(of: "test5", by: User())
+        ]
+        
+        // When
+        messageBox.append(messages)
 
+        // Then
+        XCTAssertEqual(messageBox.messages.count, 5)
+    }
+    
+    func test_append_when_lastMessage_not_exist() throws {
+        // Given
+        let messageBox = MessageBox()
+        let message = Message(of: "test", by: User())
+
+        // When
+        messageBox.append(message)
+
+        // Then
+        XCTAssertEqual(messageBox.messages.first?.text, "test")
+    }
+    
+    
 }
